2d68a7cd776bb9925397e3ad8ec1646b895eee6e
spoogle <spoogle@schlatt.com>
2007-12-17 21:38:16 +0100

    test pushing
    darcs-hash:20071217203816-8e42c-6425e8a65f1ae418de1c352a4135d7e503517479.gz

--- source/Spoogle/Build/InvIndex.hs
+++ source/Spoogle/Build/InvIndex.hs
@@ -1,4 +1,62 @@
+-- ----------------------------------------------------------------------------
+
+{- |
+  Module     : Spoogle.Query.Parser
+  Copyright  : Copyright (C) 2007 Sebastian M. Schlatt
+  License    : MIT
+
+  Maintainer : Sebastian M. Schlatt (spoogle@schlatt.com)
+  Stability  : experimental
+  Portability: untested
+  Version    : $Id$
+
+  The Spoogle query parser.
+
+-}
+
+-- ----------------------------------------------------------------------------
 
 module Spoogle.Build.InvIndex where
 
--- nothing
+import Text.XML.HXT.Arrow			-- import all stuff for parsing, validating, and transforming XML
+
+import System.IO			   		-- import the IO and commandline option stuff
+import System.Environment
+import System.Console.GetOpt
+import System.Exit
+import System
+
+import Data.Maybe
+import Data.List
+
+import Spoogle.Index.Inverted
+	( empty
+	, InvIndex
+	)
+
+
+
+main	:: IO()
+main
+    = do
+      [rc] <- runX (
+      	 	readDocument   [ (a_parse_html ,"1")
+      	 				   ] 
+					"/home/sms/ruby/ruby0.htm"
+		    >>>
+		    LA parseDoc
+		    >>>
+		     writeDocument 	[ (a_output_encoding, isoLatin1)
+				   			] 
+				   "-"
+	 	    >>>
+		    getErrStatus
+		   )
+      exitWith ( if rc >= c_err
+		 then ExitFailure 1
+		 else ExitSuccess
+	       )
+	
+
+parseDoc :: XmlTree -> [InvIndex]
+parseDoc xt = [empty]
