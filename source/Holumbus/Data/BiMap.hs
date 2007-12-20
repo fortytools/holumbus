874120eafdc2bca8c372a9209a066a12d4426ccb
t.h <t.h@gmx.info>
2007-12-20 10:36:19 +0100

    Added some more functionality to BiMap
    Some new construction functions are avaliable (empty, singleton) as well as conversion
    from lists and maps.
    
    darcs-hash:20071220093619-f854c-b2b9243531f7f76b2e998bd71713e21430377ce4.gz

--- source/Holumbus/Data/BiMap.hs
+++ source/Holumbus/Data/BiMap.hs
@@ -26,21 +26,48 @@ module Holumbus.Data.BiMap
     , lookupB
     
     -- * Construction
+    , empty
+    , singleton
     , insert
+    
+    -- * Conversion
+    , fromList
+    , fromMap
   )
 where
 
 import Data.Map (Map)
 import qualified Data.Map as M
 
-data (Ord a, Ord b) => BiMap a b = BM { a2b :: !(Map a b), b2a :: !(Map b a) } deriving (Show)
+-- | The BiMap data type from values of a to values of b and vice versa.
+data (Ord a, Ord b) => BiMap a b = BiMap { a2b :: !(Map a b)
+                                         , b2a :: !(Map b a) 
+                                         } deriving (Show)
 
+-- | Lookup the value b for another value a.
 lookupA :: (Ord a, Ord b) => a -> BiMap a b -> Maybe b
 lookupA k m = M.lookup k (a2b m)
 
+-- | Lookup the value a for another value b.
 lookupB :: (Ord a, Ord b) => b -> BiMap a b -> Maybe a
 lookupB k m = M.lookup k (b2a m)
 
-insert :: (Ord a, Ord b) => a -> b -> BiMap a b -> BiMap a b
-insert va vb m = BM (M.insert va vb $ a2b m) (M.insert vb va $ b2a m)
+-- | Create an empty BiMap.
+empty :: (Ord a, Ord b) => BiMap a b
+empty = BiMap M.empty M.empty
 
+-- | Create a BiMap with just one pair of values.
+singleton :: (Ord a, Ord b) => a -> b -> BiMap a b
+singleton va vb = BiMap (M.singleton va vb) (M.singleton vb va)
+
+-- | Insert two values into the BiMap. Existing values will be replaced with the new values.
+insert :: (Ord a, Ord b) => a -> b -> BiMap a b -> BiMap a b
+insert va vb m = BiMap (M.insert va vb $ a2b m) (M.insert vb va $ b2a m)
+
+-- | Convert a list of tuples into a BiMap.
+fromList :: (Ord a, Ord b) =>  [(a, b)] -> BiMap a b
+fromList = foldr (\(va, vb) r -> insert va vb r) empty
+
+-- | Convert a map into a BiMap.
+fromMap :: (Ord a, Ord b) => Map a b -> BiMap a b
+fromMap = M.foldWithKey (\va vb r -> insert va vb r) empty
