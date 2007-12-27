2d6e564458b474baf6bf4b2af68c1a5bc7c3399e
t.h <t.h@gmx.info>
2007-12-27 19:08:26 +0100

    First version of fuzzy set generation
    A first draft of Holumbus' unique mechanism for generation of fuzzy sets, called
    "inverse fuzzy sets". By consecutively applying a set of replacement rules, a string is
    will become more and more fuzzy.
    
    darcs-hash:20071227180826-f854c-6d19bd79a3fa2a2485b66f317f0a8c64fb5ff735.gz

--- source/Holumbus/Query/Fuzzy.hs
+++ source/Holumbus/Query/Fuzzy.hs
@@ -10,10 +10,167 @@
   Portability: portable
   Version    : 0.1
 
-  The unique Holumbus mechanism for fuzzy queries.
+  The unique Holumbus mechanism for generating fuzzy sets.
 
 -}
 
 -- ----------------------------------------------------------------------------
 
-module Holumbus.Query.Fuzzy where
+module Holumbus.Query.Fuzzy 
+  (
+  -- * Fuzzy types
+  FuzzySet
+  , FuzzyString
+  , Replacements
+  , Replacement
+  , Score
+  
+  -- * Predefined replacements
+  , englishReplacements
+  , germanReplacements
+  
+  -- * Generation
+  , fuzz
+  , fuzzWith
+  , fuzzMore
+  , fuzzMoreWith
+  , fuzzUntil
+  , fuzzUntilWith
+  )
+where
+
+import Data.List
+import Data.Function
+
+type FuzzySet = [ FuzzyString ]
+type Replacements = [ Replacement ]
+type FuzzyString = (String, Score)
+type Replacement = ((String, String), Score)
+type Score = Float
+
+-- | The default replacements to use in the functions without explicitly specified replacements.
+defaultReplacements = germanReplacements
+
+-- | Some default replacements for the english language.
+englishReplacements :: Replacements
+englishReplacements =
+  [ (("l", "ll"), 0.2)
+  , (("t", "tt"), 0.2)
+  , (("r", "rr"), 0.2)
+  ]
+
+-- | Some default replacements for the german language.
+germanReplacements :: Replacements
+germanReplacements = 
+  [ (("l", "ll"), 0.2)
+  , (("t", "tt"), 0.2)
+  , (("r", "rr"), 0.2)
+  , (("i", "ie"), 0.2)
+  , (("ei", "ie"), 0.2)
+  , (("k", "ck"), 0.2)
+
+  , (("d", "t"), 0.4)
+  , (("b", "p"), 0.4)
+  , (("g", "k"), 0.4)
+  , (("c", "k"), 0.4)
+  , (("s", "z"), 0.4)
+  , (("u", "ou"), 0.4)
+
+  , (("ü", "ue"), 0.1)
+  , (("ä", "ae"), 0.1)
+  , (("ö", "oe"), 0.1)
+  , (("ß", "ss"), 0.1)
+  ]
+
+-- | Fuzz a string usind the default replacements.
+fuzz :: String -> FuzzySet
+fuzz = fuzzWith defaultReplacements
+
+-- | Fuzz a string using an explicitly specified list of replacements.
+fuzzWith :: Replacements -> String -> FuzzySet
+fuzzWith = fuzzInternal 0.0
+
+-- | Fuzz a set of fuzzy strings even more using the default replacements (the new set of 
+-- fuzzy strings is not merged with the original set).
+fuzzMore :: FuzzySet -> FuzzySet
+fuzzMore = fuzzMoreWith defaultReplacements
+
+-- | Fuzz a set of fuzzy strings even more using an explicitly specified list of replacements.
+-- (the new set of fuzzy strings is not merged with the original set).
+fuzzMoreWith :: Replacements -> FuzzySet -> FuzzySet
+fuzzMoreWith rs fs = foldr (\(s, sc) res -> res ++ fuzzInternal sc rs s) [] fs
+
+-- | Continue fuzzing a string with the default replacements until a given score threshold is reached.
+fuzzUntil :: Score -> String -> FuzzySet
+fuzzUntil = fuzzUntilWith defaultReplacements
+
+-- | Continue fuzzing a string with the an explicitly specified list of replacements until 
+-- a given score threshold is reached.
+fuzzUntilWith :: Replacements -> Score -> String -> FuzzySet
+fuzzUntilWith rs th s = fuzzUntilWith' (fuzzLimit th 0.0 rs s)
+  where
+  fuzzUntilWith' :: FuzzySet -> FuzzySet
+  fuzzUntilWith' fs = if more == [] then fs else fs ++ (fuzzUntilWith' more)
+    where
+    -- The current score is doubled on every recursive call, because fuzziness increases exponentially.
+    more = foldr (\(s, sc) res -> res ++ fuzzLimit th (sc + sc) rs s) [] fs
+
+-- | Fuzz a string and limit the allowed score to a given threshold.
+fuzzLimit :: Score -> Score -> Replacements -> String -> FuzzySet
+fuzzLimit th sc rs s = if sc <= th then filter (\(_, ns) -> ns <= th) (fuzzInternal sc rs s) else []
+
+-- | Fuzz a string with an list of explicitly specified replacements and combine the scores
+-- with an initial score.
+fuzzInternal :: Score -> Replacements -> String -> FuzzySet
+fuzzInternal sc rs s = foldr (\r res -> res ++ applyReplacement sc rs s r ) [] rs
+
+-- | Return the first string of the replacement.
+fstRep :: Replacement -> String
+fstRep ((f, _), _) = f
+
+-- | Return the second string of the replacement.
+sndRep :: Replacement -> String
+sndRep ((_, s), _) = s
+
+-- | Applies a single replacement definition (in both directions) to a string. An initial score is
+-- combined with the new score for the replacement (calculated from the position in the string and 
+-- the scores in the list of all replacements).
+applyReplacement :: Score -> Replacements -> String -> Replacement -> FuzzySet
+applyReplacement sc rs s r = apply pre suf
+  where
+  apply :: [ String ] -> [ String ] -> FuzzySet
+  apply [] [] = []
+  apply (pr:prs) (su:sus) = apply' (fstRep r) (sndRep r) ++ apply' (sndRep r) (fstRep r) ++ apply prs sus
+    where
+    apply' :: String -> String -> FuzzySet
+    apply' tok sub = maybe [] (\rep -> [(rep, sc + calcScore (length pr) len r rs)]) (replacePosition pr su tok sub)
+  pre = init $ inits s
+  suf = init $ tails s
+  len = length s
+
+-- | Replaces a prefix in the suffix of an already splitted string.
+replacePosition :: String -> String -> String -> String -> Maybe String
+replacePosition prefix suffix tok sub = if replaced == suffix then Nothing else Just (prefix ++ replaced)
+  where
+  replaced = replaceFirst tok sub suffix
+
+-- | Calculate the final score of a replacement depending on the position in the string and the
+-- length of the string. The score is normalized to the interval [0.0, 1.0]
+calcScore :: Int -> Int -> Replacement -> Replacements -> Score
+calcScore pos len r rs = relPos * relScore
+  where
+  relPos = (l - p + 1) / l -- Normalized position (depending on the length of the string)
+  relScore = (snd r) / (snd $ maximumBy (compare `on` snd) rs) -- Normalized score (depending on maximum score)
+  p = fromIntegral pos
+  l = fromIntegral len
+
+-- | Searches a prefix and replaces it with a substitute in a list.
+replaceFirst :: Eq a => [a] -> [a] -> [a] -> [a]
+replaceFirst []       ys     zs       = ys ++ zs
+replaceFirst xs       ys     []       = []
+replaceFirst t@(x:xs) []     s@(z:zs) = if t `isPrefixOf` s then 
+                                          if x == z then replaceFirst xs [] zs else s
+                                        else s
+replaceFirst t@(x:xs) (y:ys) s@(z:zs) = if t `isPrefixOf` s then
+                                          if x == z then y : replaceFirst xs ys zs else s
+                                        else s
