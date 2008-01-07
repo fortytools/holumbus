a92aec3b34ae666e58653dee507af9391136db69
tbh <tbh@holumbus.schlatt.com>
2008-01-07 11:53:52 +0100

    Simple ranking mechanism.
    A very simple ranking of results is in place, based on just counting the number of
    occurrences.
    
    darcs-hash:20080107105352-b81cb-c97a9d6c848fdd50dfff9e8e76a8d2ee22ba6be3.gz

---
+++ source/Holumbus/Query/Ranking.hs
@@ -0,0 +1,66 @@
+-- ----------------------------------------------------------------------------
+
+{- |
+  Module     : Holumbus.Query.Ranking
+  Copyright  : Copyright (C) 2007 Timo B. Huebel
+  License    : MIT
+
+  Maintainer : Timo B. Huebel (t.h@gmx.info)
+  Stability  : experimental
+  Portability: portable
+  Version    : 0.1
+
+  The ranking mechanism for Holumbus.
+
+-}
+
+-- ----------------------------------------------------------------------------
+
+module Holumbus.Query.Ranking 
+  (
+  -- * Ranking
+  rank
+  , rankWith
+  
+  -- * Predefined document rankings
+  , docRankByCount
+  
+  -- * Predefined word rankings
+  , wordRankByCount
+  )
+where
+
+import qualified Data.Map as M
+import qualified Data.IntMap as IM
+import qualified Data.IntSet as IS
+
+import Holumbus.Query.Result
+import Holumbus.Index.Documents
+import Holumbus.Index.Common
+
+-- | The default ranking for document hits.
+docDefaultRanking :: DocId -> DocContextHits -> Score
+docDefaultRanking = docRankByCount
+
+-- | The default ranking for word hits.
+wordDefaultRanking :: Word -> WordContextHits -> Score
+wordDefaultRanking = wordRankByCount
+
+-- | Rank with the default ranking mechanism.
+rank :: Result -> Result
+rank = rankWith docDefaultRanking wordDefaultRanking
+
+-- | Rank the result with custom ranking functions.
+rankWith :: (DocId -> DocContextHits -> Score) -> (Word -> WordContextHits -> Score) -> Result -> Result
+rankWith fd fw r = Result scoredDocHits scoredWordHits
+  where
+  scoredDocHits = IM.mapWithKey (\k (_, dch) -> (fd k dch, dch)) $ docHits r
+  scoredWordHits = M.mapWithKey (\k (_, wch) -> (fw k wch, wch)) $ wordHits r
+
+-- | Rank documents by count.
+docRankByCount :: DocId -> DocContextHits -> Score
+docRankByCount _ h = fromIntegral $ M.fold (\h1 r1 -> M.fold (\h2 r2 -> IS.size h2 + r2) r1 h1) 0 h
+
+-- | Rank words by count.
+wordRankByCount :: Word -> WordContextHits -> Score
+wordRankByCount _ h = fromIntegral $ M.fold (\h1 r1 -> IM.fold (\h2 r2 -> IS.size h2 + r2) r1 h1) 0 h
