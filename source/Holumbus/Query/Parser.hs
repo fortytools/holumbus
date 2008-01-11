8307c1760bd3440ea52f48a72ba8ea98b36b19f7
tbh <tbh@holumbus.schlatt.com>
2008-01-11 11:48:39 +0100

    Parser rewritten using Parsec
    First revision of a parser rewrite using the Parsec library.
    
    darcs-hash:20080111104839-b81cb-a1071d0057b088d3f83c78cb7b1e557197b2385f.gz

--- source/Holumbus/Query/Parser.hs
+++ source/Holumbus/Query/Parser.hs
@@ -17,259 +17,141 @@
 
 -- ----------------------------------------------------------------------------
 
-module Holumbus.Query.Parser 
-  (
-  -- * Parser data types
-  Parser (P)
-
-  -- * Parsing
-  , parseQuery
-  , parseQueryWith
-  )
-where
+module Holumbus.Query.Parser (parseQuery) where
 
 import Holumbus.Query.Syntax
+import Text.ParserCombinators.Parsec
+import Data.Either
 
-import Char
-import Control.Monad
---import Text.ParserCombinators.Parsec
-
--- | The parser monad.
-data Parser a = P (String -> [(a, String)])
-
-instance Monad Parser where
-  return v = P (\inp -> [(v, inp)])
-  p >>= f  = P (\inp -> case parse p inp of
-                         [] -> []
-                         (v, out):_ -> (parse (f v) out))
-
--- | The choice operator, read as "or else".
-(+++) :: Parser a -> Parser a -> Parser a
-p +++ q = P( \inp -> case parse p inp of
-                       [] -> parse q inp
-                       (v, out):_ -> [(v, out)])
-
--- | Parse a string with the default parser.
-parseQuery :: String -> [(Query, String)]
-parseQuery = parse query
-
--- | Parse a string with a custom parser. This allows for highly customized querylanguages.
-parseQueryWith :: Parser Query -> String -> [(Query, String)]
-parseQueryWith = parse
-
--- | Apply a parser to an input string.
-parse :: Parser a -> String -> [(a, String)]
-parse (P p) inp = p inp
-
--- | The failure parser, always returns a failure.
-failure :: Parser a
-failure = P (\_ -> [])
-
--- | Parse a single item (a character).
-item :: Parser Char
-item = P (\inp -> case inp of
-                    [] -> []
-                    (x:xs) -> [(x, xs)])
-
--- | Apply some predicate to an item.
-sat :: (Char -> Bool) -> Parser Char
-sat p = do
-          x <- item
-          if (p x) then return x else failure
-
--- | Parse an alphanumeric character.
-alphanum :: Parser Char
-alphanum = sat isAlphaNum
-
--- | Parse a character that is valid in a query (not one of the reserved symbols).
-querychar :: Parser Char
-querychar = sat isQueryChar
-
--- | Parse a character that is valid in a phrase (everything except the phrase delimiter).
-phrasechar :: Parser Char
-phrasechar = sat isPhraseChar
-
--- | Is a character valid in a phrase (i.e. not the phrase delimiter)?
-isPhraseChar :: Char -> Bool
-isPhraseChar c | c == '"'  = False
-               | otherwise = True
-
--- | Is a character valid in a query (i.e. not one of the reserved symbols)?
-isQueryChar :: Char -> Bool
-isQueryChar c | c == '"'   = False
-              | c == '('   = False
-              | c == ')'   = False
-              | isSpace c  = False
-              | otherwise  = True
-
--- | Parse a specific character.
-char :: Char -> Parser Char
-char x = sat (== x)
-
--- | Parse a string (a sequence of characters).
-string :: String -> Parser String
-string [] = return []
-string (x:xs) = do
-                  char x
-                  string xs
-                  return (x:xs)
-
--- | Apply a parser several times in sequence.
-many :: Parser a -> Parser [a]
-many p = manyOne p +++ return []
-
--- | Apply a parser several times but at leas one time in sequence.
-manyOne :: Parser a -> Parser [a]
-manyOne p = do 
-              v <- p
-              vs <- many p
-              return (v:vs)
-
--- | Parse a abritrary amount of whitespace.
-space :: Parser ()
-space = do
-          many (sat isSpace)
-          return ()
-
--- | Parse a token, which is something not equal to whitespace that may be surrounded by any whitespace.
-token :: Parser a -> Parser a
-token p = do
-            space
-            v <- p
-            space
-            return v
-
--- | Parse a symbol, which is a specific string treated as token (may be surrounded by whitespace).
-symbol :: String -> Parser String
-symbol xs = token (string xs)
-
--- | Parse a single context name.
-context :: Parser String
-context = token (manyOne alphanum)
-
--- | Parse a list of context specifiers, which are context names separated by commas.
-contexts :: Parser [String]
-contexts =  do
-              x <- context
-              do
-                symbol ","
-                xs <- contexts
-                return (x:xs)
-                +++ return [x]
-
--- | Parse a phrase, which is anything surrounded by the phrase delimiters.
-phrase :: Parser String
-phrase = token phrase'
+-- | Parse a query.
+parseQuery :: String -> Either String Query
+parseQuery = result . (parse query "")
   where
-    phrase' :: Parser String
-    phrase' = do
-                char '"'
-                x <- manyOne phrasechar
-                char '"'
-                return x
+  result (Left err) = Left (show err)
+  result (Right q)  = Right q
 
--- | Parse a single word, which may not contain reserved symbols.
-word :: Parser String
-word = token word'
-  where  
-    word' :: Parser String
-    word' = do
-              x <- manyOne querychar
-              return x
-
--- | Just for convenience, the basic query consists of and terms.
+-- | A query may always be surrounded by whitespace
 query :: Parser Query
 query = andQuery
 
--- | Parse an AND-query.
+-- | Parse an and query.
 andQuery :: Parser Query
-andQuery = do
-             t <- orQuery
-             do
-               andOp
-               q <- query
-               return (BinQuery And t q)
-               +++ return t
+andQuery = do t <- orQuery
+              spaces
+              do andOp
+                 spaces
+                 q <- query
+                 return (BinQuery And t q)
+                 <|> return t
 
--- | Parse an OR-query.
+-- | Parse an or query.
 orQuery :: Parser Query
-orQuery = do
-            t <- notQuery
-            do
-              orOp
-              q <- query
-              return (BinQuery Or t q)
-              +++ return t
+orQuery = do t <- notQuery
+             spaces
+             do orOp
+                spaces
+                q <- orQuery
+                return (BinQuery Or t q)
+                <|> return t
 
--- | Parse a NOT-query.
+-- | Parse a negation.
 notQuery :: Parser Query
-notQuery = do
-             notOp
-             q <- contextQuery
-             return (Negation q)
-             +++ contextQuery
+notQuery = do spaces
+              notQuery' <|> contextQuery
+  where
+  notQuery' = do notOp
+                 spaces
+                 q <- contextQuery
+                 return (Negation q)
 
--- | Parse a context-query.
+-- | Parse a context query.
 contextQuery :: Parser Query
-contextQuery = do
-                 c <- contexts
-                 symbol ":"
-                 t <- parQuery
-                 return (Specifier c t)
-                 +++ parQuery
+contextQuery = do try contextQuery' <|> parQuery
+  where
+  contextQuery' = do c <- contexts
+                     spaces
+                     char ':'
+                     spaces
+                     t <- parQuery
+                     return (Specifier c t)
 
--- | Parse a query enclosed in round brackets.
+-- | Parse a query surrounded by parentheses.
 parQuery :: Parser Query
-parQuery = do
-             symbol "("
-             q <- query
-             symbol ")"
-             return q
-             +++ caseQuery
+parQuery = do parQuery' <|> caseQuery
+  where
+  parQuery' = do char '('
+                 q <- query
+                 char ')'
+                 return q
 
--- | Parse a case-sensitive query.
+-- | Parse a case sensitive query.
 caseQuery :: Parser Query
-caseQuery = do
-              symbol "!"
-              do
-                (phraseQuery CasePhrase) +++ (wordQuery CaseWord)
-              +++ fuzzyQuery
+caseQuery = do caseQuery' <|> fuzzyQuery
+  where
+  caseQuery' = do char '!'
+                  spaces
+                  (phraseQuery CasePhrase <|> wordQuery CaseWord)
 
 -- | Parse a fuzzy query.
 fuzzyQuery :: Parser Query
-fuzzyQuery = do
-               symbol "~"
-               wordQuery FuzzyWord
-               +++ (phraseQuery Phrase) +++ (wordQuery Word)
+fuzzyQuery = do fuzzyQuery' <|> phraseQuery Phrase <|> wordQuery Word
+  where
+  fuzzyQuery' = do char '~'
+                   spaces
+                   wordQuery FuzzyWord
 
--- | Parse a terminal word query.
+-- | Parse a word query.
 wordQuery :: (String -> Query) -> Parser Query
-wordQuery f = do
-                w <- word
-                return (f w)
+wordQuery c = do w <- word
+                 return (c w)
 
--- | Parse a terminal phrase query.
+-- | Parse a phrase query.
 phraseQuery :: (String -> Query) -> Parser Query
-phraseQuery f = do
-                  p <- phrase
-                  return (f p)
+phraseQuery c = do p <- phrase
+                   return (c p)
 
--- | Parse an AND-operator ("AND" or just whitespace).
+-- | Parse an and operator.
 andOp :: Parser ()
-andOp = do
-        symbol "AND"
-        return ()
-        +++ space
+andOp = do string "AND" 
+           return ()
+           <|> spaces 
 
--- | Parse an OR-operator ("OR").
+-- | Parse an or operator.
 orOp :: Parser ()
-orOp = do
-         symbol "OR"
-         return ()
-
--- | Parse a NOT-operator ("NOT").
-notOp :: Parser ()
-notOp = do
-          symbol "NOT"
+orOp = do string "OR"
           return ()
 
+-- | Parse a not operator.
+notOp :: Parser ()
+notOp = do string "NOT"
+           return ()
+
+-- | Parse a word.
+word :: Parser String
+word = many1 wordChar
+
+-- | Parse a phrase.
+phrase :: Parser String
+phrase = do char '"'
+            p <- many1 phraseChar
+            char '"'
+            return p
+
+-- | Parse a character of a word.
+wordChar :: Parser Char
+wordChar = noneOf "\")( "
+
+-- | Parse a character of a phrases.
+phraseChar :: Parser Char
+phraseChar = noneOf "\""
+
+-- | Parse a list of contexts.
+contexts :: Parser [String]
+contexts = context `sepBy1` (char ',')
+            
+-- | Parse a context.
+context :: Parser String
+context = do spaces 
+             c <- (many1 alphaNum)
+             spaces
+             return c
