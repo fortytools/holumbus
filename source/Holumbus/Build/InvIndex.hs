aed437b31ee52ec3d2713a047820fbc3ef7374e4
sms <sms@holumbus.schlatt.com>
2008-01-11 17:28:05 +0100

    fix & foxy
    darcs-hash:20080111162805-23a6a-349cbcb29256b81696a38958161fd0501852ef30.gz

--- source/Holumbus/Build/InvIndex.hs
+++ source/Holumbus/Build/InvIndex.hs
@@ -5,12 +5,12 @@
   Copyright  : Copyright (C) 2007 Sebastian M. Schlatt
   License    : MIT
 
-  Maintainer : Sebastian M. Schlatt (spoogle@schlatt.com)
+  Maintainer : Sebastian M. Schlatt (sms@holumbus.schlatt.com)
   Stability  : experimental
   Portability: untested
   Version    : $Id$
 
-  The Holumbus query parser.
+  The Holumbus sailor.
 
 -}
 
@@ -26,37 +26,96 @@ import System.Console.GetOpt
 import System.Exit
 import System
 
+import Holumbus.Data.StrMap (StrMap)
+import qualified Holumbus.Data.StrMap as SM
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+import qualified Data.IntMap as IM
+
+import qualified Data.IntSet as IS
+
+
 import Data.Maybe
 import Data.List
 
 import Holumbus.Index.Inverted
-	( empty
-	, InvIndex
-	)
+import Holumbus.Index.Common
+
+
+-- import MapReduce.Basic
+import qualified MapReduce.Basic as MAPRED
+
+import qualified Misc.Fold
+import qualified Misc.Unfold
+
+type Dict = Map
+
+
+r f = readDocument [(a_parse_html ,"1")] f 
+
+{-
+r1 = r (testDocs !! 1)
+ra = map r testDocs
+
+dest = "/home/sms/workspace/ruby.idx"
+idxfile = "/home/sms/haskell/spoogle/www.haskell.org.idx.xml"
 
 
 
-main	:: IO()
-main
-    = do
-      [rc] <- runX (
-      	 	readDocument   [ (a_parse_html ,"1")
-      	 				   ] 
-					"/home/sms/ruby/ruby0.htm"
-		    >>>
-		    LA parseDoc
-		    >>>
-		     writeDocument 	[ (a_output_encoding, isoLatin1)
-				   			] 
-				   "-"
-	 	    >>>
-		    getErrStatus
-		   )
-      exitWith ( if rc >= c_err
-		 then ExitFailure 1
-		 else ExitSuccess
-	       )
-	
+stdOpts = [(a_validate, v_0)]
 
-parseDoc :: XmlTree -> [InvIndex]
-parseDoc xt = [empty]
+nn src = nn' src stdOpts 
+
+nn' src opts = readDocument opts src
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wordDocPair = MAPRED.mapReduce wdpMAP wdpREDUCE
+
+wdpMAP :: a -> String -> [(String, (a, Int))] 
+wdpMAP k v =  map (flipFlop k) (zip [1..] (Misc.Fold.words v))
+  where flipFlop k (pos, word) = (word, (k, pos))
+
+wdpREDUCE :: String -> [(Int, Int)] -> Maybe (IM.IntMap IS.IntSet)
+wdpREDUCE _ l = Just $ foldl theFunc IM.empty l                     -- [(doc, pos)]
+  where 
+    theFunc im (doc, pos) =
+      if IM.member doc im
+        then IM.adjust (IS.insert pos) doc im
+        else IM.insert doc (IS.singleton pos) im
+
+wdp =
+       print
+     $ wordDocPair
+     $ simple_INTput
+
+
+simple_INTput =
+       M.insert 2 "appreciate the unfold"
+     $ M.insert 1 "fold the fold"
+     $ M.empty
+
+
+testDocs ::  [String]
+testDocs = ["/home/sms/ruby/ruby0.htm"	
+           ,"/home/sms/ruby/ruby1.htm"
+           ,"/home/sms/ruby/ruby2.htm"
+           ,"/home/sms/ruby/ruby3.htm"
+           ,"/home/sms/ruby/ruby4.htm"
+           ,"/home/sms/ruby/ruby5.htm"
+           ,"/home/sms/ruby/ruby6.htm"
+           ]-}
