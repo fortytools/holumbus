fd03e7bd7542d3119a5c119d78c0de0f97d5fa6e
tbh <tbh@holumbus.schlatt.com>
2008-01-15 23:24:11 +0100

    Adjusted fuzzy test
    Adjusted fuzzy test to use the new configuration capabilities.
    
    darcs-hash:20080115222411-b81cb-3c248512b466d8a9410d4cd7ea71271ed820f774.gz

--- test/FuzzyTest.hs
+++ test/FuzzyTest.hs
@@ -25,52 +25,40 @@ import Test.HUnit
 replaceTests :: Test
 replaceTests = TestList 
   [ TestCase (assertEqual "Simple replace"
-  [("acb", 2/3), ("bac", 1.0), ("fbc", 1.0)]
-  (toList $ fuzzWith [(("a", "f"), 1.0)] "abc"))
+  [("fbc", 1.0)]
+  (toList $ fuzz (FuzzyConfig True False 1.0 [(("a", "f"), 1.0)]) "abc"))
 
   , TestCase (assertEqual "Replace in both directions"
-  [("aac", 2/3), ("acb", 2/3), ("bac", 1.0), ("bbc", 1.0)]
-  (toList $ fuzzWith [(("a", "b"), 1.0)] "abc"))
+  [("aac", 2/3), ("bbc", 1.0)]
+  (toList $ fuzz (FuzzyConfig True False 1.0 [(("a", "b"), 1.0)]) "abc"))
 
   , TestCase (assertEqual "Calculating scores depending on position"
-  [("abcda", 0.2), ("abced", 0.4), ("abdce", 0.6), ("acbde", 0.8), ("bacde", 1.0), ("ebcde", 1.0)]
-  (toList $ fuzzWith [(("a", "e"), 1.0)] "abcde"))
+  [("abcda", 0.2), ("ebcde", 1.0)]
+  (toList $ fuzz (FuzzyConfig True False 1.0 [(("a", "e"), 1.0)]) "abcde"))
 
   , TestCase (assertEqual "Calculating scores depending on position and weight"
-  [("abcda", 0.1), ("abced", 0.4), ("ebcde", 0.5), ("abbde", 0.6), ("abdce", 0.6), ("acbde", 0.8), ("accde", 0.8), ("bacde", 1.0)]
-  (toList $ fuzzWith [(("a", "e"), 0.4), (("b", "c"), 0.8)] "abcde"))
+  [("abcda", 0.1), ("ebcde", 0.5), ("abbde", 0.6), ("ebcda", 0.7), ("abbda", 0.8), ("accde", 0.8), ("accda", 1.0)]
+  (toList $ fuzz (FuzzyConfig True False 1.0 [(("a", "e"), 0.4), (("b", "c"), 0.8)]) "abcde"))
 
   , TestCase (assertEqual "Balancing weights correctly"
-  [("abcda", 0.1), ("abced", 0.4), ("ebcde", 0.5), ("abbde", 0.6), ("abdce", 0.6), ("acbde", 0.8), ("accde", 0.8), ("bacde", 1.0)]
-  (toList $ fuzzWith [(("a", "e"), 1.0), (("b", "c"), 2.0)] "abcde"))
+  [("abcda", 0.1), ("ebcde", 0.5), ("abbde", 0.6), ("ebcda", 0.7), ("abbda", 0.8), ("accde", 0.8), ("accda", 1.0)]
+  (toList $ fuzz (FuzzyConfig True False 1.0 [(("a", "e"), 1.0), (("b", "c"), 2.0)]) "abcde"))
   ]
 
 swapTests :: Test
 swapTests = TestList 
   [ TestCase (assertEqual "Correctly swap all characters"
   [("acb", 2/3), ("bac", 1.0)]
-  (toList $ fuzzWith [] "abc"))
+  (toList $ fuzz (FuzzyConfig False True 1.0 []) "abc"))
 
   , TestCase (assertEqual "Correctly calculating scores"
   [("abced", 0.4), ("abdce", 0.6), ("acbde", 0.8), ("bacde", 1.0)]
-  (toList $ fuzzWith [] "abcde"))
+  (toList $ fuzz (FuzzyConfig False True 1.0 []) "abcde"))
 
   , TestCase (assertEqual "Swap alot"
-  [("abcdefghji", 0.2), ("abcdefgihj", 0.3), ("abcdefhgij", 0.4), ("abcdegfhij", 0.5), ("abcdfeghij", 0.6), 
-  ("abcedfghij", 0.7), ("abdcefghij", 0.8), ("acbdefghij", 0.9), ("bacdefghij", 1.0)]
-  (toList $ fuzzWith [] "abcdefghij"))
-  ]
-
-fuzzMoreTests :: Test
-fuzzMoreTests = TestList
-  [ TestCase (assertEqual "More swapping"
-  [("abc", 4/3), ("bca", 1.0 + 2/3), ("cab", 1.0 + 2/3)]
-  (toList $ fuzzMoreWith [] $ fuzzWith [] "abc"))
-
-  , TestCase (assertEqual "More replacing"
-  [("abc", 4/3), ("aeb", 4/3), ("bae", 1.0 + 1/3), ("fbe", 1.0 + 1/3), ("bca", 1.0 + 2/3), 
-   ("cab", 1.0 + 2/3), ("fcb", 1.0 + 2/3), ("bfc", 2.0)]
-  (toList $ fuzzMoreWith [(("c", "e"), 1.0)] $ fuzzWith [(("a", "f"), 1.0)] "abc"))
+  [("abcdefghji", 0.2), ("abcdefgihj", 0.3), ("abcdefhgij", 0.4), ("abcdegfhij", 0.5)
+  , ("abcdfeghij", 0.6), ("abcedfghij", 0.7)]
+  (toList $ fuzz (FuzzyConfig False True 0.7 []) "abcdefghij"))
   ]
 
 allTests :: Test
@@ -78,5 +66,4 @@ allTests = TestLabel "Fuzzy tests" $
   TestList 
   [ TestLabel "Replace tests" replaceTests
   , TestLabel "Swap tests" swapTests 
-  , TestLabel "Fuzz more tests" fuzzMoreTests
   ]
