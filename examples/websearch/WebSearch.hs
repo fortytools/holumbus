b948b9912a0288307afe3ac78d825ef57dbbf16a
tbh <tbh@holumbus.schlatt.com>
2008-01-17 10:00:28 +0100

    Generating HTML on the server
    The WebSearch example now generates the HTML itself, making the whole application
    more useble with different browsers (even IE works now to some extent).
    
    darcs-hash:20080117090028-b81cb-b6164383109ba20a0aeffc1b01f1cc578af5a86f.gz

--- examples/websearch/WebSearch.hs
+++ examples/websearch/WebSearch.hs
@@ -17,15 +17,21 @@
 
 -- ----------------------------------------------------------------------------
 
-{-# OPTIONS -fglasgow-exts -farrows #-}
+{-# OPTIONS -fglasgow-exts -farrows -fno-warn-type-defaults #-}
 
 module Network.Server.Janus.Shader.WebSearch where
 
-import Text.XML.HXT.Arrow
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.IntMap as IM
 
---import Holumbus.Index.Common
+import Text.XML.HXT.Arrow
+import Text.XML.HXT.DOM.Unicode
+
 import Holumbus.Index.Combined
 import Holumbus.Index.Inverted
+import Holumbus.Index.Documents
+import Holumbus.Index.Common
 
 import Holumbus.Query.Syntax
 import Holumbus.Query.Parser
@@ -34,21 +40,23 @@ import Holumbus.Query.Result
 import Holumbus.Query.Ranking
 import Holumbus.Query.Fuzzy
 
-import Network.Server.Janus.Core
+import Network.Server.Janus.Core (Shader, ShaderCreator)
+import qualified Network.Server.Janus.Core as J
 import Network.Server.Janus.XmlHelper
 import Network.Server.Janus.JanusPaths
+
+import System.Time
 
 import Control.Concurrent  -- For the global MVar
 
 import Network.CGI         -- For decoding URI-encoded strings
 
 -- Status information of query processing.
-type StatusResult = (Status, Result)
-type Status = (String, Int, Float, Int, Float)
+type StatusResult = (String, Result)
 
 websearchShader :: ShaderCreator
-websearchShader = mkDynamicCreator $ proc (_, _) -> do
-  tmp <- arrIO $ loadFromFile -< "indexes/vl.xml" -- Should be configurable (from Context)
+websearchShader = J.mkDynamicCreator $ proc (_, _) -> do
+  tmp <- arrIO $ loadFromFile -< "indexes/sd.xml" -- Should be configurable (from Context)
   mix <- arrIO $ newMVar -< Inv tmp
   returnA -< websearchService mix
 
@@ -56,47 +64,148 @@ websearchService :: MVar AnyIndex -> Shader
 websearchService mix = proc inTxn -> do
   idx      <- arrIO $ readMVar                                             -< mix
   request  <- getValDef (_transaction_http_request_cgi_ "@query") ""       -< inTxn
-  response <- writeString <<< (genError ||| genResult) <<< (arrParseQuery) -< (urlDecode request, idx)
+  arrLogRequest                                                            -< inTxn
+  response <- writeString <<< (genError ||| genResult) <<< (arrParseQuery) -< (request, idx)
   setVal _transaction_http_response_body response                          -<< inTxn    
     where
-    writeString = pickleStatusResult >>> (writeDocumentToString [(a_indent, v_1), (a_output_encoding, isoLatin1)])
+    writeString = pickleStatusResult >>> (writeDocumentToString [(a_no_xml_pi, v_1), (a_output_encoding, utf8)])
     pickleStatusResult = xpickleVal xpStatusResult
 
 arrParseQuery :: ArrowXml a => a (String, AnyIndex) (Either (String, AnyIndex) (Query, AnyIndex))
-arrParseQuery = arr $ (\(r, i) -> either (\m -> Left (m, i)) (\q -> Right (q, i)) (parseQuery r))
+arrParseQuery = (first arrDecode)
+                >>>
+                (arr $ (\(r, i) -> either (\m -> Left (m, i)) (\q -> Right (q, i)) (parseQuery r)))
 
-genResult :: ArrowXml a => a (Query, AnyIndex) (Status, Result)
-genResult = (arr $ (\(q, i) -> (makeQuery i q, i)))
-            >>>
-            (first $ arr $ rank)
-            >>>
-            (arr $ (\(r, i) -> annotateResult i r))
-            >>>
-            (arr $ (\r -> (setResult r defaultStatus , r)))
+arrDecode :: Arrow a => a String String
+arrDecode = arr $ fst . utf8ToUnicode . urlDecode
 
+arrLogRequest :: JanusArrow J.Context XmlTree ()
+arrLogRequest = proc inTxn -> do
+  remHost <- getValDef (_transaction_tcp_remoteHost) ""                -< inTxn
+  rawRequest <- getValDef (_transaction_http_request_cgi_ "@query") "" -< inTxn
+  decodedRequest <- arrDecode                                          -< rawRequest
+  unixTime <- arrIO $ (\_ -> getClockTime)                             -< ()
+  currTime <- arr $ calendarTimeToString . toUTCTime                   -< unixTime
+  arrIO $ putStrLn -< (currTime ++ " - " ++ remHost ++ " - " ++ rawRequest ++ " - " ++ decodedRequest)
+
+genResult :: ArrowXml a => a (Query, AnyIndex) (String, Result)
+genResult = ifP (\(q, _) -> checkWith ((> 1) . length) q)
+              ((arr $ (\(q, i) -> (makeQuery i q, i)))
+              >>>
+              (first $ arr $ rank)
+              >>>
+              (arr $ (\(r, i) -> annotateResult i r))
+              >>>
+              (arr $ (\r -> (msgSuccess r , r))))
+              
+              (arr $ (\(_, _) -> ("Please enter some more characters.", emptyResult)))
+
+msgSuccess :: Result -> String
+msgSuccess r = if sd == 0 then "Nothing found yet." 
+               else "Found " ++ (show sd) ++ " " ++ ds ++ " and " ++ (show sw) ++ " " ++ cs ++ "."
+                 where
+                 sd = sizeDocHits r
+                 sw = sizeWordHits r
+                 ds = if sd == 1 then "document" else "documents"
+                 cs = if sw == 1 then "completion" else "completions"
+
+-- | This is where the magic happens!
 makeQuery :: AnyIndex -> Query -> Result
-makeQuery i q = if checkWith ((> 1) . length) q then
-                processQuery cfg i (optimize q)
-                else emptyResult
+makeQuery i q = processQuery cfg i (optimize q)
                   where
                   cfg = ProcessConfig [] (FuzzyConfig True True 1.0 germanReplacements)
 
-genError :: ArrowXml a => a (String, AnyIndex) (Status, Result)
-genError = arr $ (\(msg, _) -> (setMessage msg defaultStatus, emptyResult))
+genError :: ArrowXml a => a (String, AnyIndex) (String, Result)
+genError = arr $ (\(msg, _) -> (msg, emptyResult))
 
 xpStatusResult :: PU StatusResult
-xpStatusResult = xpElem "holumbus" (xpPair xpStatus xpResult)
+xpStatusResult = xpElem "div" $ xpAddFixedAttr "id" "result" $ xpPair xpStatus xpResultHtml
 
-xpStatus :: PU Status
-xpStatus = xpElem "status" (xp5Tuple (xpElem "message" xpPrim)
-           (xpElem "doccount" xpPrim) (xpElem "docscore" xpPrim)
-           (xpElem "wordcount" xpPrim) (xpElem "wordscore" xpPrim))
+xpStatus :: PU String
+xpStatus = xpElem "div" $ xpAddFixedAttr "id" "status" xpText
 
-defaultStatus :: Status
-defaultStatus = ("", 0, 0.0, 0, 0.0)
+-- The HTML Result pickler
 
-setResult :: Result -> Status -> Status
-setResult r (m, _, _, _, _) = (m, sizeDocs r, maxScoreDocs r, sizeWords r, maxScoreWords r)
+xpResultHtml :: PU Result
+xpResultHtml = xpWrap (\((_, wh), dh) -> Result dh wh, \r -> ((maxScoreWordHits r, wordHits r), docHits r)) 
+               (xpPair xpWordHitsHtml xpDocHitsHtml)
 
-setMessage :: String -> Status -> Status
-setMessage m (_, dh, ds, wh, ws) = (m, dh, ds, wh, ws)
+-- | Wrapping something in a <div> element with id attribute.
+xpDivId :: String -> PU a -> PU a
+xpDivId i p = xpElem "div" (xpAddFixedAttr "id" i p)
+
+-- | Set the class of the surrounding element.
+xpClass :: String -> PU a -> PU a
+xpClass c p = xpAddFixedAttr "class" c p
+
+xpAppend :: String -> PU a -> PU a
+xpAppend t p = xpWrap (\(v, _) -> v, \v -> (v, t)) (xpPair p xpText)
+
+xpPrepend :: String -> PU a -> PU a
+xpPrepend t p = xpWrap (\(_, v) -> v, \v -> (t, v)) (xpPair xpText p)
+
+-- | The HTML pickler for the document hits. Will be sorted by score.
+xpDocHitsHtml :: PU DocHits
+xpDocHitsHtml = xpDivId "documents" (xpWrap (IM.fromList, toListSorted) (xpList xpDocHitHtml))
+  where
+  toListSorted = reverse . L.sortBy (compare `on` (docScore . fst . snd)) . IM.toList -- Sort by score
+  xpDocHitHtml = xpElem "p" $ xpClass "document" $ xpDocInfoHtml
+
+xpDocInfoHtml :: PU (DocId, (DocInfo, DocContextHits))
+xpDocInfoHtml = xpWrap (docFromHtml, docToHtml) (xpTriple xpTitleHtml xpContextsHtml xpURIHtml)
+
+docToHtml :: (DocId, (DocInfo, DocContextHits)) -> ((URI, Title), DocContextHits, URI)
+docToHtml (_, (DocInfo (title, uri) _, dch)) = ((uri, title), dch, uri)
+
+docFromHtml :: (Document, DocContextHits, URI) -> (DocId, (DocInfo, DocContextHits))
+docFromHtml ((uri, title), dch, _) = (0, (DocInfo (title, uri) 0.0, dch))
+
+xpTitleHtml :: PU (URI, Title)
+xpTitleHtml = xpElem "div" $ xpClass "title" $ xpElem "a" $ xpClass "link" $ (xpPair (xpAttr "href" xpText) xpText)
+
+xpContextsHtml :: PU DocContextHits
+xpContextsHtml = xpElem "div" $ xpClass "contexts" $ xpWrap (M.fromList, M.toList) (xpList xpContextHtml)
+
+xpContextHtml :: PU (Context, DocWordHits)
+xpContextHtml = xpPair (xpElem "span" $ xpClass "context" $ xpAppend ": " $ xpText) xpWordsHtml
+
+xpWordsHtml :: PU DocWordHits
+xpWordsHtml = xpWrap (M.fromList, M.toList) (xpList (xpPair (xpAppend " " $ xpText) xpZero))
+
+xpURIHtml :: PU String
+xpURIHtml = xpElem "div" $ xpClass "uri" $ xpText
+
+xpWordHitsHtml :: PU (Score, WordHits)
+xpWordHitsHtml = xpDivId "words" $ xpElem "p" $ xpClass "cloud" $ xpWrap (fromListSorted, toListSorted) (xpList xpWordHitHtml)
+  where
+  fromListSorted _ = (0.0, M.empty)
+  toListSorted (s, wh) = map (\a -> (s, a)) $ L.sortBy (compare `on` fst) $ M.toList wh -- Sort by word
+  xpWordHitHtml = xpWrap (wordFromHtml, wordToHtml) (xpWordHtml)
+
+wordToHtml :: (Score, (Word, (WordInfo, WordContextHits))) -> ((String, Word), ((Score, Score), Word))
+wordToHtml (m, (w, (WordInfo ts s, _))) = ((head ts, w), ((s, m), w))
+
+wordFromHtml :: ((String, Word), ((Score, Score), Word)) -> (Score, (Word, (WordInfo, WordContextHits)))
+wordFromHtml ((t, _), ((s, m), w)) = (m, (w, (WordInfo [t] s, M.empty)))
+
+xpWordHtml :: PU ((String, Word), ((Score, Score), Word))
+xpWordHtml = xpAppend " " $ xpElem "a" $ xpClass "cloud" $ xpPair xpLink xpScore
+
+xpLink :: PU (String, Word)
+xpLink = xpAttr "href" $ xpPair (xpPrepend "javascript:replaceInQuery('" $ xpAppend "','" xpText) (xpAppend "')" $ xpText)
+
+xpScore :: PU ((Score, Score), Word)
+xpScore = xpElem "span" $ xpPair (xpAttr "class" $ xpWrap (scoreFromHtml, scoreToHtml) xpText) xpText
+
+weightScore :: Score -> Score -> Score -> Score -> Score
+weightScore mi ma to v = ma - ((to - v) / to) * (ma - mi)
+
+scoreToHtml :: (Score, Score) -> String
+scoreToHtml (v, top) = "cloud" ++ (show $ round (weightScore 1 9 top v))
+
+scoreFromHtml :: String -> (Score, Score)
+scoreFromHtml _ = (0.0, 0.0)
+
+-- This is a fix for GHC 6.6.1 (from 6.8.1 on, this is avaliable in module Data.Function)
+on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
+op `on` f = \x y -> f x `op` f y
--- examples/websearch/wwwpages/holumbus.js
+++ examples/websearch/wwwpages/holumbus.js
@@ -1,13 +1,8 @@
 var lastPress = (new Date()).getTime();
-var poll;
 
-function showThrobber () {
-	document.getElementById("throbber").style.display = "inline";
-}
-
-function hideThrobber () {
-	document.getElementById("throbber").style.display = "none";
-}
+document.observe("dom:loaded", function() {
+	$("throbber").hide();
+});
 
 function tryProcessQuery () {
 	currentPress = (new Date()).getTime();
@@ -25,147 +20,29 @@ function forceProcessQuery () {
 }
 
 function processQuery () {
-	query = document.getElementById("querytext").value;
+	var query = $("querytext").value;
 	if (query.length > 1) {
-		showThrobber();
-		new Ajax.Request("holumbus.xml?query=" + query,
+		$("throbber").show();
+		new Ajax.Request("result.html?query=" + query,
 		{
 			method:'get',
 			onSuccess: function(transport) {
 				lastXMLResult = transport.responseXML;
 				lastTXTResult = transport.responseText;
-			  displayResult(transport.responseXML);
+			  displayResult(transport.responseText);
 			},
 			onFailure: function(){ alert('Something went wrong...') }
 		});
 	}
 }
 
+function replaceInQuery (needle, substitute) {
+	$("querytext").value = $("querytext").value.gsub(needle, substitute);
+	processQuery();
+}
+
 function displayResult (result) {
-	var msg = result.getElementsByTagName("message")[0];
-
-	var docHits = result.getElementsByTagName("dochits")[0];
-	var wordHits = result.getElementsByTagName("wordhits")[0];
-
-	var noDocHits = result.getElementsByTagName("doccount")[0].firstChild.data;
-	var maxDocScore = result.getElementsByTagName("docscore")[0].firstChild.data;
-	var noWordHits = result.getElementsByTagName("wordcount")[0].firstChild.data;
-	var maxWordScore = result.getElementsByTagName("wordscore")[0].firstChild.data;
-
-	if (noDocHits == 0) {
-		var txt = "Nothing found yet.";
-	}
-	else {
-		var txt = "Found " + noDocHits + " documents and " + noWordHits + " completions.";
-	}
-	var stats = document.createTextNode(txt);
-	document.getElementById("stats").replaceChild(stats, document.getElementById("stats").firstChild);
-
-	displayDocHits(docHits, noDocHits, maxDocScore);
-	displayWordHits(wordHits, noWordHits, maxWordScore);
+	$("result").replace(result);
 	
-	hideThrobber();
-}
-
-function displayWordHits (hits, count, score) {
-	var words = hits.getElementsByTagName("word");
-
-	var cloud = document.createElement("p");
-	cloud.setAttribute("id", "cloud");
-
-	var capacity = (0.9 * (window.innerWidth - 250)).round();
-	var i = 0;
-	var chars = 0;
-
-	while (i < words.length && chars < capacity) {
-		var word = words[i];
-		var w = word.getAttribute("w");
-		var s = word.getAttribute("score");
-		var size = weightScore(1, 9, s, score).round();
-		var node = document.createElement("span");
-		node.setAttribute("class", "cloud" + size);
-		var text = document.createTextNode(w);
-		node.appendChild(text);
-		
-		cloud.appendChild(node);
-		cloud.appendChild(document.createTextNode(" "));
-		
-		chars += w.length * size;
-		i++;
-	}
-	
-	if (chars >= capacity) {
-		cloud.appendChild(document.createTextNode("..."));
-	}
-	
-	var container = document.getElementById("words");
-	container.replaceChild(cloud, document.getElementById("cloud"));
-}
-
-function displayDocHits (hits, count, score) {
-	var docs = hits.getElementsByTagName("doc");
-	
-	var list = document.createElement("div");
-	list.setAttribute("id", "list");
-	
-	for (var i = (docs.length - 1); i >= 0; i--) {
-		var doc = docs[i];
-		var href = doc.getAttribute("href");
-
-		var node = document.createElement("p");
-		node.setAttribute("class", "entry");
-		
-		var link = document.createElement("a");
-		link.setAttribute("class", "link");
-		link.setAttribute("href", href);
-		link.appendChild(document.createTextNode(doc.getAttribute("title")));
-		var title = document.createElement("div");
-		title.setAttribute("class", "title");
-		title.appendChild(link);
-
-		var contexts = createContextNode(doc.getElementsByTagName("context"));
-		
-		var uri = document.createElement("div");
-		uri.setAttribute("class", "uri");
-		uri.appendChild(document.createTextNode(href));
-		
-		node.appendChild(title);
-		node.appendChild(contexts);
-		node.appendChild(uri);
-		
-		list.appendChild(node);
-		list.appendChild(document.createTextNode(" "));
-	}
-	
-	var container = document.getElementById("results");
-	container.replaceChild(list, document.getElementById("list"));
-}
-
-function createContextNode(contexts) {
-	var node = document.createElement("div");
-	node.setAttribute("class", "contexts");
-
-	for (var i = 0; i < contexts.length; i++) {
-		var context = contexts[i];
-		var name = document.createElement("span");
-		name.setAttribute("class", "context");
-		var txt = context.getAttribute("name");
-		var upcase = txt.substr(0, 1).toUpperCase() + txt.substr(1).toLowerCase();
-		name.appendChild(document.createTextNode(upcase + ":"));
-		node.appendChild(name);
-		node.appendChild(document.createTextNode(" "));
-		
-		var words = context.getElementsByTagName("word");
-		for (var j = 0; j < words.length; j++) {
-			var word = words[j];
-			node.appendChild(document.createTextNode(word.getAttribute("w") + " "));
-		}
-	}
-	
-	return node;
-}
-
-// Transforms val from the range 0.0 - top to a corresponding value in the range min - max
-function weightScore (min, max, val, top) {
-	return max - ((top - val) / top) * (max - min);
+	$("throbber").hide();
 }
