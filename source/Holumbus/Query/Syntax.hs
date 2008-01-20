e704e9db444aabc8b51f23f4e9ac9a5be90ced9b
tbh <tbh@holumbus.schlatt.com>
2008-01-20 16:47:59 +0100

    Renamed the "Filter" operator to "But"
    Renamed the "Filter" operator for shortness and more compliance with general IR terms.
    
    darcs-hash:20080120154759-b81cb-b5bce8eabeaf8e9963f32519f80a42976098e204.gz

--- source/Holumbus/Query/Syntax.hs
+++ source/Holumbus/Query/Syntax.hs
@@ -21,7 +21,7 @@ module Holumbus.Query.Syntax
   (
   -- * Query data types
   Query (Word, Phrase, CaseWord, CasePhrase, FuzzyWord, Specifier, Negation, BinQuery)
-  , BinOp (And, Or, Filter)
+  , BinOp (And, Or, But)
 
   -- * Optimizing
   , optimize
@@ -43,16 +43,16 @@ data Query = Word       String
            deriving (Eq, Show)
 
 -- | A binary operation.
-data BinOp = And | Or | Filter deriving (Eq, Show)
+data BinOp = And | Or | But deriving (Eq, Show)
 
 -- | Transforms all @(BinQuery And q1 q2)@ where one of @q1@ or @q2@ is a @Negation@ into
 -- @BinQuery Filter q1 q2@ or @BinQuery Filter q2 q1@ respectively.
 optimize :: Query -> Query
-optimize (BinQuery And q1 (Negation q2)) = BinQuery Filter (optimize q1) (optimize q2)
-optimize (BinQuery And (Negation q1) q2) = BinQuery Filter (optimize q2) (optimize q1)
+optimize (BinQuery And q1 (Negation q2)) = BinQuery But (optimize q1) (optimize q2)
+optimize (BinQuery And (Negation q1) q2) = BinQuery But (optimize q2) (optimize q1)
 optimize (BinQuery And q1 q2) = BinQuery And (optimize q1) (optimize q2)
 optimize (BinQuery Or q1 q2) = BinQuery Or (optimize q1) (optimize q2)
-optimize (BinQuery Filter q1 q2) = BinQuery Or (optimize q1) (optimize q2)
+optimize (BinQuery But q1 q2) = BinQuery Or (optimize q1) (optimize q2)
 optimize (Negation q) = Negation (optimize q)
 optimize (Specifier cs q) = Specifier cs (optimize q)
 optimize q = q
