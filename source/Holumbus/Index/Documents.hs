29c41612ac052f0f5462a58b8544a0a503209842
tbh <tbh@holumbus.schlatt.com>
2008-01-24 01:10:17 +0100

    Binary file support
    Added support to encode the documents table into some binary format.
    
    darcs-hash:20080124001017-b81cb-c1a037112f1b237a4049161b72b4777a2d7c972e.gz

--- source/Holumbus/Index/Documents.hs
+++ source/Holumbus/Index/Documents.hs
@@ -31,6 +31,7 @@ module Holumbus.Index.Documents
   , insert
 
   -- * Query
+  , size
   , lookupId
   , lookupURI
   , lastId
@@ -51,7 +52,10 @@ import qualified Data.Map as M
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
 
-import Holumbus.Control.Sequence
+import Data.Word
+import Data.Binary
+
+import Control.Parallel.Strategies
 
 -- | The table which is used to map a document to an artificial id and vice versa.
 data Documents     = Documents { idToDoc   :: !(IntMap Document)
@@ -69,15 +73,21 @@ type URI           = String
 -- | The title of a document.
 type Title         = String
 
-instance DeepSeq Documents where
-  deepSeq (Documents i2d d2i lid) b = deepSeq i2d $ deepSeq d2i $ deepSeq lid b
+instance NFData Documents where
+  rnf (Documents i2d d2i lid) = rnf i2d `seq` rnf d2i `seq` rnf lid
 
 instance XmlPickler Documents where
   xpickle =  xpWrap convertDoctable (xpWrap (IM.fromList, IM.toList) (xpList xpDocumentWithId))
     where
-    convertDoctable = (\itd -> Documents itd (itd2dti itd) (lastId itd), \(Documents itd _ _) -> itd)
-    itd2dti = IM.foldWithKey (\i (_, s2) r -> M.insert s2 i r) M.empty
+    convertDoctable = (\itd -> Documents itd (idToDoc2docToId itd) (lastId itd), \(Documents itd _ _) -> itd)
     xpDocumentWithId = xpElem "doc" (xpPair (xpAttr "id" xpPrim) xpDocument)
+
+instance Binary Documents where
+  put (Documents i2d _ _) = do
+                            put i2d
+  get = do
+        i2d <- get
+        return (Documents i2d (idToDoc2docToId i2d) (lastId i2d))
 
 -- | The XML pickler for a single document.
 xpDocument :: PU Document
@@ -90,9 +100,17 @@ xpDocument = xpPair xpTitle xpURI
 xpDocuments :: PU Documents
 xpDocuments = xpElem "documents" $ xpickle
 
+-- | Construct the inverse map from the original map.
+idToDoc2docToId :: IntMap Document -> Map URI DocId
+idToDoc2docToId = IM.foldWithKey (\i (_, s2) r -> M.insert s2 i r) M.empty
+
 -- | Create an empty table.
 emptyDocuments :: Documents
 emptyDocuments = Documents IM.empty M.empty 0
+
+-- | Returns the number of documents in the table.
+size :: Documents -> Int
+size d = IM.size (idToDoc d)
 
 -- | Query the @idToDoc@ part of the document table for the last id.
 lastId :: IntMap Document -> Int
@@ -107,12 +125,15 @@ lookupId :: DocId -> Documents -> Maybe Document
 lookupId = (. idToDoc) . IM.lookup
 
 -- | Insert a document into the table. Returns a tuple of the id for that document and the new table.
+-- If a document with the same URI is already present, its id will be returned with unchanged @Documents@.
 insertWithId :: Document -> Documents -> (DocId, Documents)
-insertWithId d@(_, u) ds = (newId, Documents newIdToDoc newDocToId newId)
+insertWithId d@(_, u) ds = maybe reallyInsert (\oldId -> (oldId, ds)) (lookupURI u ds)
   where
-  newIdToDoc = IM.insert newId d (idToDoc ds)
-  newDocToId = M.insert u newId (docToId ds)
-  newId = (lastDocId ds) + 1
+  reallyInsert = (newId, Documents newIdToDoc newDocToId newId)
+    where
+    newIdToDoc = IM.insert newId d (idToDoc ds)
+    newDocToId = M.insert u newId (docToId ds)
+    newId = (lastDocId ds) + 1
 
 -- | Insert a document into the table.
 insert :: Document -> Documents -> Documents
