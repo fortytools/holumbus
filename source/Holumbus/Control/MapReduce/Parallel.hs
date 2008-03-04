ea7ec8e1616d29ba0d9b6362dd9d089a9fd5a342
sms <sms@holumbus.org>
2008-03-04 01:27:51 +0100

    solved warnings and removed deprecated functions
    darcs-hash:20080304002751-ff21e-44d31f5a367561d52f701a9ee112ba8ac0b5f477.gz

--- source/Holumbus/Control/MapReduce/Parallel.hs
+++ source/Holumbus/Control/MapReduce/Parallel.hs
@@ -25,7 +25,7 @@ import Text.XML.HXT.Arrow
 
 import           Holumbus.Control.Registry
 
-import           Data.Map (Map,empty,insertWith,mapWithKey,filterWithKey)
+import           Data.Map (Map,empty,insertWith) -- ,mapWithKey,filterWithKey)
 import qualified Data.Map    as M
 import           Data.Maybe (isJust, fromJust)
 
@@ -36,166 +36,93 @@ type Dict   = Map
 
 -- ----------------------------------------------------------------------------
 
+-- | MapReduce Computations
 mapReduce :: (Ord k2) =>
                 Int
              -> (k1 ->  v1  -> IO [(k2, v2)])
              -> (k2 -> [v2] -> IO (Maybe v3))
              -> [(k1, v1)]
              -> IO (M.Map k2 v3)
-mapReduce maxWorkers mAP rEDUCE input
+mapReduce maxWorkers mapFunction reduceFunction input
   = do
-    ps  <- return (max 1 ((length input) `div` maxWorkers)) 
+    
+    -- split the input data into pieces of approximately the same size
+    ps       <- return (max 1 ((length input) `div` maxWorkers)) 
     runX (traceMsg 0 ("          MapReduce partition size: " ++ show ps))
-    pin <- return ( partitionList ps input ) 
+    pin      <- return ( partitionList ps input ) 
 
+    -- parallel map phase
     runX (traceMsg 0 ("                    mapPerKey " ))
-    mapped <- parallelMap mAP pin 
+    mapped   <- parallelMap mapFunction pin 
     
+    -- grouping of data gained in the map phase
     runX (traceMsg 0 ("                    groupByKey " ))
-    grouped <- return  (groupByKey mapped)  
+    grouped  <- return  (groupByKey mapped)  
     
+    -- reduce phase
     runX (traceMsg 0 ("                    reduceByKey "))
---    return M.empty
-    reducePerKey rEDUCE grouped
+    reducePerKey reduceFunction grouped
     
 -- ----------------------------------------------------------------------------
 
+-- | executes the map phase of a MapReduce computation as a parallel computation.
+--   it is not very smart so far since the input data is split in the beginning
+--   and there is no work load balancing during the computation
 parallelMap :: (k1 ->  v1  -> IO [(k2, v2)]) -> [[(k1, v1)]] -> IO [(k2, v2)]
-parallelMap mAP pl
+parallelMap mapFunction pl
   = do 
     result          <- newMVar []
-    workers         <- startWorkers (runMapTask result mAP) pl
+    workers         <- startWorkers (runMapTask result mapFunction) pl
     waitForWorkers  workers
     res             <- takeMVar result
     return res
 
 
 runMapTask :: MVar [(k2,v2)] -> (k1 ->  v1  -> IO [(k2, v2)]) -> [(k1, v1)] -> IO ()
-runMapTask mv mAP input 
+runMapTask mv mapFunction input 
   = do
-    mrMap   <- mapPerKey (uncurry mAP) input 
+    mrMap   <- mapPerKey (uncurry mapFunction) input 
     current <- takeMVar mv
     combined <- return (mrMap ++ current)
     putMVar mv combined   
     
+-- ----------------------------------------------------------------------------
+
 mapPerKey :: ((k1,v1)  -> IO [(k2, v2)]) -> [(k1, v1)] -> IO [(k2,v2)]
-mapPerKey mAP []     = do return ([])
-mapPerKey mAP (x:xs)
+mapPerKey _ []     = do return ([])
+mapPerKey mapFunction (x:xs)
   = do
-    the_x  <- mAP x
-    the_xs <- mapPerKey mAP xs
+    the_x  <- mapFunction x
+    the_xs <- mapPerKey mapFunction xs
     return $! (the_x ++ the_xs)
     -- maybe way nicer with mapM
-    
-reducePerKey :: (Ord k2) => (k2 -> [v2] -> IO(Maybe v3)) -> Map k2 [v2] -> IO (Map k2 v3)
-reducePerKey rEDUCE inmap
-  = rpk (uncurry rEDUCE) (M.toList inmap) M.empty
-    where
-      rpk :: (Ord k2) => ((k2, [v2]) -> IO(Maybe v3)) -> [(k2, [v2])] -> Map k2 v3 -> IO (Map k2 v3)
-      rpk rEDUCE []      result = do return (result)
-      rpk rEDUCE toProcess result
-        = do
-          first   <- return $ head toProcess
-          applied <- rEDUCE first
-          if isJust applied
-            then rpk rEDUCE (drop 1 toProcess) (M.union result (M.singleton (fst first) (fromJust applied)))
-            else rpk rEDUCE (drop 1 toProcess) result
-          
-           
-       -- return M.empty
---    x <- mapWithKey rEDUCE inmap
-    
---    x <- mapWithKey rEDUCE inmap
---      mapWithKey unJust            -- 3. Transform type to remove Maybe
---    . filterWithKey (const isJust) -- 2. Remove entries with value Nothing
---    . mapWithKey rEDUCE            -- 1. Apply *'\reduce'* per key
---   where 
---         unJust _ (Just v)   = v       -- Transforms optional into non-optional type
---         unJust _ Nothing    = error "guarded by filter"    
-
-red :: a -> [b] -> IO (Maybe c)
-red _ _ = do return Nothing
-
--- ----------------------------------------------------------------------------
-
-
-mapReduce' :: (Ord k2) =>
-                Int
-             -> ((k1, v1) -> IOSLA (XIOState ()) XmlTree (k2, v2))
-             -> (k2 -> [v2] -> Maybe v3)
-             -> [(k1, v1)]
-             -> IO (M.Map k2 v3)
-mapReduce' maxWorkers mAP rEDUCE input 
-  = do
-       -- calculate partition size and partition input
-    pl  <- return (max 1 ((length input) `div` maxWorkers)) 
-    runX (traceMsg 0 ("          MapReduce partition size: " ++ show pl))
-    pin <- return ( partitionList pl input ) 
-    
-    runX (traceMsg 0 ("                    mapPerKey " ))
-    mall' <- parallelMap' mAP pin 
-    
-    runX (traceMsg 0 ("                    groupByKey " ))
-    m'' <- return  (groupByKey mall')  
-    
-    runX (traceMsg 0 ("                    reduceByKey "))
-    return (reducePerKey' rEDUCE m'')
-    
--- ----------------------------------------------------------------------------
-
-parallelMap' :: ((k1, v1) -> IOSLA (XIOState ()) XmlTree a) -> [[(k1, v1)]] -> IO [a]
-parallelMap' mAP pl
-  = do 
-    result          <- newMVar []
-    workers         <- startWorkers (runMapTask' result mAP) pl
-    waitForWorkers  workers
-    res             <- takeMVar result
-    return res
-
-
-runMapTask' :: MVar [c] -> ((k1, v1) -> IOSLA (XIOState ()) XmlTree c) -> [(k1, v1)] -> IO ()
-runMapTask' mv mAP input 
-  = do
-    mrMap   <- runX (mapPerKey' mAP input) 
-    current <- takeMVar mv
-    combined <- return $! (head mrMap) ++ current
-    putMVar mv combined        
-
--- ----------------------------------------------------------------------------
-
-
-mapPerKey' :: (ArrowList a) => (a1 -> a b c) -> [a1] -> a b [c]
-mapPerKey' mAP input = listA  $ catA $ 
-      map mAP input
     
 groupByKey :: (Ord k2) => [(k2, v2)] -> Dict k2 [v2]
 groupByKey = foldl insert empty
   where
     insert dict (k2,v2) = insertWith (++) k2 [v2] dict
-
-reducePerKey' :: (Ord k2) => (k2 -> v2 -> Maybe v3) -> Map k2 v2 -> Map k2 v3
-reducePerKey' rEDUCE =
-      mapWithKey unJust       -- 3. Transform type to remove Maybe
-    . filterWithKey isItJust    -- 2. Remove entries with value Nothing
-    . mapWithKey rEDUCE       -- 1. Apply *'\reduce'* per key
-   where 
-         isItJust = const isJust
---         isJust _ (Just _)   = True    -- Keep entries of this form
---         isJust _ Nothing    = False   -- Remove entries of this form
-         unJust _ (Just v)   = v       -- Transforms optional into non-optional type
-         unJust _ Nothing    = error "guarded by filter"
+    
+reducePerKey :: (Ord k2) => (k2 -> [v2] -> IO(Maybe v3)) -> Map k2 [v2] -> IO (Map k2 v3)
+reducePerKey reduceFunction inmap
+  = rpk (uncurry reduceFunction) (M.toList inmap) M.empty
+    where
+      rpk :: (Ord k2) => ((k2, [v2]) -> IO(Maybe v3)) -> [(k2, [v2])] -> Map k2 v3 -> IO (Map k2 v3)
+      rpk _ [] result = do return (result)
+      rpk reduceFunction' toProcess result
+        = do
+          firstP  <- return $ head toProcess
+          applied <- reduceFunction' firstP
+          if isJust applied
+            then rpk reduceFunction' (drop 1 toProcess) (M.union result (M.singleton (fst firstP) (fromJust applied)))
+            else rpk reduceFunction' (drop 1 toProcess) result
+          
 
 -- ----------------------------------------------------------------------------
-
+    
 -- | partition the list of input data into a list of input data lists of
 --   approximately the same length
 partitionList :: Int -> [a] -> [[a]]
 partitionList _ [] = []
 partitionList count l  = [take count l] ++ (partitionList count (drop count l)) 
 
--- ----------------------------------------------------------------------------
-
--- | reduce function to skip the reduce phase in a MapReduce computation
-noReduce :: (Ord k2) => k2 -> [v2] -> Maybe v2 
-noReduce _ = Just . head         
          
