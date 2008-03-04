c930d3fc2a3c8afd79d2d166746bc95d5fa91c98
sms <sms@holumbus.org>
2008-03-04 01:34:16 +0100

    added module for holumbus xml filters
    darcs-hash:20080304003416-ff21e-713f5e408590be025a5da7681b5ca5fb26ff3014.gz

---
+++ source/Holumbus/Build/XmlFilter.hs
@@ -0,0 +1,39 @@
+-- ----------------------------------------------------------------------------
+
+{- |
+  Module     : Holumbus.Build.XmlFilter
+  Copyright  : Copyright (C) 2008 Sebastian M. Schlatt
+  License    : MIT
+  
+  Maintainer : Sebastian M. Schlatt (sms@holumbus.org)
+  Stability  : experimental
+  Portability: portable
+  Version    : 0.1
+  
+  This module provides functions to manipulate XmlTrees while building an index
+
+-}
+
+-- ----------------------------------------------------------------------------
+
+module Holumbus.Build.XmlFilter where
+
+import           Text.XML.HXT.Arrow     -- import all stuff for parsing, validating, and transforming XML
+
+
+flattenElementsByType :: ArrowXml a => String -> a XmlTree XmlTree
+flattenElementsByType elemName
+    = processTopDown
+        ( getChildren
+          `when`
+          ( isElem >>> hasName elemName )
+        )
+        
+flattenElementsByXpath :: ArrowXml a => String -> a XmlTree XmlTree
+flattenElementsByXpath _ = this -- processXPathTreesss Text.XML.HXT.Arrow.XmlNodeSet
+      
+removeElementsByXPath :: ArrowXml a => String -> a XmlTree XmlTree
+removeElementsByXPath = processXPathTrees none        
+        
+        
+        
