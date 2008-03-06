0111621760ab7941cf936d98f034580d942c0be7
tbh <tbh@holumbus.org>
2008-03-06 22:29:59 +0100

    Fixed signature functions
    The normalization of signatures did not handle parentheses correctly and the stripping
    of whitespace ignored some separating character sequences.
    
    darcs-hash:20080306212959-67f31-088decc52bc021149ce54ee18bff189fdb07a753.gz

--- source/Holumbus/Utility.hs
+++ source/Holumbus/Utility.hs
@@ -31,12 +31,15 @@ normalizeSignature = join "->" . (replaceTypes M.empty ['a'..'z']) . split "->" 
   replaceTypes _ _ [] = []
   replaceTypes v t (x:xs) = let (nv, ut, rx) = replace in rx:(replaceTypes nv ut xs)
     where
-    replace = let ut = [head t] in maybe (M.insert x ut v, tail t, ut) (\n -> (v, t, n)) (M.lookup x v)
+    replace = let ut = [head t] in maybe (M.insert r ut v, tail t, ut) (\n -> (v, t, n)) (M.lookup r v)
+      where r = stripWith (\c -> (c == '(') || (c == ')')) x
 
 -- | Strip unneeded whitespace from a signature, e.g. @String -> Map k a -> Int@ will be transformed
 -- to @String->Map k a->Int@.
 stripSignature :: String -> String
-stripSignature = join "->" . map strip . split "->"
+stripSignature = sep "->" . sep "(" . sep ")" . sep "." . sep "=>"
+  where
+  sep s = join s . map strip . split s
 
 -- | Split a string into seperate strings at a specific character sequence.
 split :: Eq a => [a] -> [a] -> [[a]]
