a74bab73ceb49493f49252d5338965a96d1793b4
tbh <tbh@holumbus.org>
2008-03-07 00:05:28 +0100

    Added a first draft of a partially-persistent index.
    A simple version of an index with externalized occurrences which can be created from an
    existing inverted index.
    
    darcs-hash:20080306230528-67f31-02a7051edf92588ee28278872c9cff99922e7b88.gz

---
+++ source/Holumbus/Index/Persistent.hs
@@ -0,0 +1,128 @@
+-- ----------------------------------------------------------------------------
+
+{- |
+  Module     : Holumbus.Index.Persistent
+  Copyright  : Copyright (C) 2008 Timo B. Huebel
+  License    : MIT
+  
+  Maintainer : Timo B. Huebel (tbh@holumbus.org)
+  Stability  : experimental
+  Portability: portable
+  Version    : 0.1
+  
+  A persistent version of the inverted index, which keeps the dictionary
+  in memory but stores the Occurrences on disk. For extensive documentation 
+  of the index interface, see class 'HolIndex' in "Holumbus.Index.Common".
+
+-}
+
+-- ----------------------------------------------------------------------------
+
+module Holumbus.Index.Persistent 
+(
+  -- * Persistent index types
+  PersIndex (..)
+  , makePersistent
+)
+where
+
+import System.IO
+import System.IO.Unsafe
+import Control.Exception
+import Control.Monad
+
+import Control.Parallel.Strategies
+
+import Data.Maybe
+import Data.Binary
+
+import qualified Data.ByteString.Lazy as B
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+import Holumbus.Index.Common
+import Holumbus.Index.Compression
+
+import Holumbus.Index.Inverted (InvIndex (InvIndex))
+
+import Holumbus.Data.StrMap (StrMap)
+import qualified Holumbus.Data.StrMap as SM
+
+-- | The index consists of a table which maps documents to ids and a number of index parts.
+data PersIndex = PersIndex 
+  { indexParts :: Parts     -- ^ The parts of the index, each representing one context.
+  , occurrences :: FilePath -- ^ Path to a file containing temorary data.
+  } deriving (Show, Eq)
+
+-- | The index parts are identified by a name, which should denote the context of the words.
+type Parts       = Map Context Part
+-- | The index part is the real inverted index. Words are mapped to a file pointer.
+type Part        = StrMap (Integer, Int)
+
+instance HolIndex PersIndex where
+  sizeWords = M.fold ((+) . SM.size) 0 . indexParts
+  contexts = map fst . M.toList . indexParts
+
+  allWords i c = map (\(w, o) -> (w, retrieveOcc (occurrences i) o)) $ SM.toList $ getPart c i
+  prefixCase i c q = map (\(w, o) -> (w, retrieveOcc (occurrences i) o)) $ SM.prefixFindWithKey q $ getPart c i
+  prefixNoCase i c q = map (\(w, o) -> (w, retrieveOcc (occurrences i) o)) $ SM.prefixFindNoCaseWithKey q $ getPart c i
+  lookupCase i c q = map (\o -> (q, retrieveOcc (occurrences i) o)) $ maybeToList (SM.lookup q $ getPart c i)
+  lookupNoCase i c q = map (\(w, o) -> (w, retrieveOcc (occurrences i) o)) $ SM.lookupNoCase q $ getPart c i
+
+  mergeIndexes _ _ = undefined
+  substractIndexes _ _ = undefined
+
+  insertOccurrences _ _ _ _ = undefined
+  deleteOccurrences _ _ _ _ = undefined
+
+  splitByContexts _ = undefined
+  splitByDocuments _ = undefined
+  splitByWords _ = undefined
+
+  updateDocuments _ _ = undefined
+
+instance Binary PersIndex where
+  put (PersIndex parts occ) = put parts >> put occ
+  get = liftM2 PersIndex get get
+
+instance NFData PersIndex where
+  rnf (PersIndex parts occ) = rnf parts `seq` rnf occ
+
+-- | Create a persistent index from an inverted index.
+makePersistent :: FilePath -> InvIndex -> PersIndex
+makePersistent f (InvIndex parts) = PersIndex (store parts) f
+  where
+  store = M.map (SM.map (storeOcc f . inflateOcc))
+
+-- | Return a part of the index for a given context.
+getPart :: Context -> PersIndex -> Part
+getPart c i = fromMaybe SM.empty (M.lookup c $ indexParts i)
+
+-- | Read occurrences from disk through unsafe IO.
+retrieveOcc :: FilePath -> (Integer, Int) -> Occurrences
+retrieveOcc f o = unsafePerformIO (handle (\_ -> return emptyOccurrences) (readOccurrences o f))
+
+-- | Write occurrences to disk through unsafe IO.
+storeOcc :: FilePath -> Occurrences -> (Integer, Int)
+storeOcc f o = unsafePerformIO (handle (\_ -> return (0,0)) (writeOccurrences o f))
+
+readOccurrences :: (Integer, Int) -> FilePath -> IO Occurrences
+readOccurrences (pos, len) f = 
+  do
+  h <- openBinaryFile f ReadMode
+  hSeek h AbsoluteSeek pos
+  r <- B.hGet h len
+  hClose h
+  return $! decode r
+
+writeOccurrences :: Occurrences -> FilePath -> IO (Integer, Int)
+writeOccurrences o f =
+  do
+  h <- openBinaryFile f ReadWriteMode
+  hSeek h SeekFromEnd 0
+  pos <- hTell h
+  r <- return $! encode o
+  B.hPut h r
+  hClose h
+  return (pos, fromIntegral $ B.length r)
