f769cc5efeaf8ba50e592230f6578b44db1a0e27
sms <sms@holumbus.org>
2008-03-09 19:08:28 +0100

    Added dummy file for the Hayoo Indexer
    darcs-hash:20080309180828-ff21e-f83c3a287d797eae3a85c150bc42d4ab321a41ed.gz

---
+++ examples/hayoo/indexer/HayooIndexer.hs
@@ -0,0 +1,21 @@
+-- ----------------------------------------------------------------------------
+
+{- |
+  Module     : HayooIndexer
+  Copyright  : Copyright (C) 2008 Sebastian M. Schlatt
+  License    : MIT
+
+  Maintainer : Sebastian M. Schlatt (sms@holumbus.org)
+  Stability  : experimental
+  Portability: untested
+  Version    : 0.1
+
+  Indexer for the Haskell API Documentation Search Engine Hayoo!
+
+-}
+
+-- ----------------------------------------------------------------------------
+
+{-# OPTIONS -farrows -fglasgow-exts #-}
+
+module Main where
