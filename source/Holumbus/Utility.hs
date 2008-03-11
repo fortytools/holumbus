0dc2bef238b1edd27b619e7c40a51b9fb595aeee
sms <sms@holumbus.org>
2008-03-11 21:07:50 +0100

    Removed functions that were moved to another module
    Some Functions for signatures where moved to the HayooHelper file in the Hayoo Example folder
    
    darcs-hash:20080311200750-ff21e-c23ed0ad47526cac1bf8f423f6e845b595222b72.gz

--- source/Holumbus/Utility.hs
+++ source/Holumbus/Utility.hs
@@ -21,25 +21,6 @@ module Holumbus.Utility where
 import Data.Char
 
 import qualified Data.List as L
-import qualified Data.Map as M
-
--- | Normalizes a Haskell signature, e.g. @String -> Int -> Int@ will be transformed to 
--- @a->b->b@. All whitespace will be removed from the resulting string.
-normalizeSignature :: String -> String
-normalizeSignature = join "->" . (replaceTypes M.empty ['a'..'z']) . split "->" . filter (not . isSpace)
-  where
-  replaceTypes _ _ [] = []
-  replaceTypes v t (x:xs) = let (nv, ut, rx) = replace in rx:(replaceTypes nv ut xs)
-    where
-    replace = let ut = [head t] in maybe (M.insert r ut v, tail t, ut) (\n -> (v, t, n)) (M.lookup r v)
-      where r = stripWith (\c -> (c == '(') || (c == ')')) x
-
--- | Strip unneeded whitespace from a signature, e.g. @String -> Map k a -> Int@ will be transformed
--- to @String->Map k a->Int@.
-stripSignature :: String -> String
-stripSignature = sep "->" . sep "(" . sep ")" . sep "." . sep "=>"
-  where
-  sep s = join s . map strip . split s
 
 -- | Split a string into seperate strings at a specific character sequence.
 split :: Eq a => [a] -> [a] -> [[a]]
