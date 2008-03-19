78f7034604641114fa60b695a4d47dede8f12abe
sms <sms@holumbus.org>
2008-03-19 22:14:52 +0100

    Minor optical changes
    The output is no diagram-like.
    This time for the right file.
    
    darcs-hash:20080319211452-ff21e-32ed4b35a19c0c9d908a46415ef798d50931c7d3.gz

--- examples/utilities/Hebalizer.hs
+++ examples/utilities/Hebalizer.hs
@@ -21,6 +21,7 @@ module Main where
 import Holumbus.Utility
 import Holumbus.Control.MapReduce.Parallel
 import Text.XML.HXT.Arrow
+import Numeric
 
 import Data.Map hiding (split, map, filter)
 
@@ -30,10 +31,17 @@ main
     logs    <- readFile ("/srv/www/holumbus.schlatt.com/janus/build/hayoo.log")
     entries <- return $! filter (/= "") (split "\n" logs)
     stats   <- mapReduce 1 processLog makeStatistics (zip (repeat 42) entries)
-    runX (mkHtml (toList stats) >>> writeDocument [] "/srv/www/holumbus.schlatt.com/htdocs/stats.html")
+    withMax <- return $! getMaximums (toList stats)
+    runX (mkHtml withMax >>> writeDocument [] "/srv/www/holumbus.schlatt.com/htdocs/stats.html")
     return ()
        
-mkHtml :: ArrowXml a => [(String, [(String, Int)])] -> a b XmlTree
+getMaximums :: [(String, [(String, Int)])] -> [(String, Int, [(String, Int)])]
+getMaximums = map addMaximum
+  where
+  addMaximum (sectionName, values) = (sectionName, getMaximum values, values)
+  getMaximum values = maximum (map snd values)
+       
+mkHtml :: ArrowXml a => [(String, Int, [(String, Int)])] -> a b XmlTree
 mkHtml l = 
      root [] [
           selem "html" 
@@ -51,22 +59,28 @@ mkHtml l =
             
               
     
-mkSection :: ArrowXml a => (String, [(String, Int)]) -> a b XmlTree
-mkSection (sectionName,values) =
+mkSection :: ArrowXml a => (String, Int, [(String, Int)]) -> a b XmlTree
+mkSection (sectionName, maxVal, values) =
     mkelem "div" [sattr "id" sectionName] 
       [ selem "h2" [ constA sectionName >>> mkText] 
       , selem "table"
         [ selem "tbody"
-          (map mkTr values)         
+          (map (mkTr maxVal) values)         
         ]
       ] 
     where
-      mkTr (value, count) =
+      mkTr maxVal (value, count) =
         selem "tr"
           [ selem "td" [ constA value        >>> mkText]
           , selem "td" [ constA (show count) >>> mkText]
+          , selem "td" [ mkelem "img" [ sattr "src" "http://www.holumbus.com/blau.gif"
+                                      , sattr "height" "12"
+                                      , sattr "width" (show $ round ((fromIntegral count) * (fromIntegral maxImageWidth) / (fromIntegral maxVal)))
+                                      ]
+                                      [] 
+                       ]
           ]
-      
+      maxImageWidth = 300
 
 processLog :: Int -> String -> IO [(String, String)]             -- the MAP
 processLog _ entry 
