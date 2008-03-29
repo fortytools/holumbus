c36b4c42d7730bc6744ef1cd79aeb027dac64598
tbh <tbh@holumbus.org>
2008-03-29 20:07:42 +0100

    Moved Hebalizer
    Moved Hebalizer to Hayoo directory and added support for command line arguments.
    
    darcs-hash:20080329190742-67f31-8648bca8278c1a796fa7142a4b22d0b22d8515a9.gz

--- examples/hayoo/stats/Hebalizer.hs
+++ examples/hayoo/stats/Hebalizer.hs
@@ -1,7 +1,7 @@
 -- ----------------------------------------------------------------------------
 
 {- |
-  Module     : Convert
+  Module     : Hebalizer
   Copyright  : Copyright (C) 2008 Sebastian M. Schlatt
   License    : MIT
 
@@ -10,7 +10,7 @@
   Portability: untested
   Version    : 0.1
 
-  Create HTML Statistics from Hayoo log files
+  Create HTML Statistics from Hayoo log files.
 
 -}
 
@@ -18,23 +18,65 @@
 
 module Main where
 
+import System.Exit
+import System.IO
+import System.Environment
+import System.Console.GetOpt
+
+import Text.XML.HXT.Arrow
+
 import Holumbus.Utility
 import Holumbus.Control.MapReduce.Parallel
-import Text.XML.HXT.Arrow
-import Numeric
 
-import Data.Map hiding (split, map, filter)
+import qualified Data.List as L
+import Data.Map hiding (split, map, filter, null)
+
+data Flag = Input String 
+          | Output String 
+          | Version 
+          | Help deriving (Show, Eq)
+
+version :: String
+version = "0.1"
 
 main :: IO ()
-main 
-  = do
-    logs    <- readFile ("/srv/www/holumbus.schlatt.com/janus/build/hayoo.log")
-    entries <- return $! filter (/= "") (split "\n" logs)
-    stats   <- mapReduce 1 processLog makeStatistics (zip (repeat 42) entries)
-    withMax <- return $! getMaximums (toList stats)
-    runX (mkHtml withMax >>> writeDocument [] "/srv/www/holumbus.schlatt.com/htdocs/stats.html")
-    return ()
+main = do
+  argv <- getArgs
+  flags <- commandLineOpts argv
+
+  if Version `elem` flags then (putStrLn version) >> (exitWith ExitSuccess) else return ()
+  if Help `elem` flags then usage [] >> (exitWith ExitSuccess) else return ()  
+
+  inp <- return (filter isInput flags)
+  if L.null inp then usage ["No input file given!\n"] else return ()
+  if length inp > 1 then usage ["Only one input file allowed!\n"] else return ()
+
+  out <- return (filter isOutput flags)
+  if L.null out then usage ["No output file given!\n"] else return ()
+  if length out > 1 then usage ["Only one output file allowed!\n"] else return ()
+ 
+  startup (head inp) (head out)
+  return ()
        
+startup :: Flag -> Flag -> IO ()
+startup (Input inp) (Output out) = do  
+  logs    <- readFile inp
+  entries <- return $! filter (/= "") (split "\n" logs)
+  stats   <- mapReduce 1 processLog makeStatistics (zip (repeat 42) entries)
+  withMax <- return $! getMaximums (toList stats)
+  runX (mkHtml withMax >>> writeDocument [(a_indent, "1")] out)
+  return ()
+
+startup _ _ = usage ["Internal error!\n"]
+
+isInput :: Flag -> Bool
+isInput (Input _) = True
+isInput _ = False
+       
+isOutput :: Flag -> Bool
+isOutput (Output _) = True
+isOutput _ = False
+
 getMaximums :: [(String, [(String, Int)])] -> [(String, Int, [(String, Int)])]
 getMaximums = map addMaximum
   where
@@ -99,4 +141,26 @@ makeStatistics _ values
       theFunc m v =
          insertWith (+) v 1 m   
    
+usage :: [String] -> IO a
+usage errs = if null errs then do
+             hPutStrLn stdout use
+             exitWith ExitSuccess
+             else do
+             hPutStrLn stderr (concat errs ++ "\n" ++ use)
+             exitWith (ExitFailure (-1))
+  where
+  header = "Hebalizer - Generate statistics from Hayoo! log files.\n\n" ++
+           "Usage: Hebalizer [OPTIONS]"
+  use    = usageInfo header options
 
+commandLineOpts :: [String] -> IO [Flag]
+commandLineOpts argv = case getOpt Permute options argv of
+                       (o, [], []  ) -> return o
+                       (_, _, errs) -> usage errs
+
+options :: [OptDescr Flag]
+options = [ Option ['i'] ["input"] (ReqArg Input "FILE") "Loads log data from FILE"
+          , Option ['o'] ["output"] (ReqArg Output "FILE") "Outputs statistics to FILE"
+          , Option ['V'] ["version"] (NoArg Version) "Output version and exit"
+          , Option ['?'] ["help"] (NoArg Help) "Output this help and exit"
+          ]
