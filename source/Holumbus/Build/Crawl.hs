a1f5d82024072731f99c5ae7151c2458e068930e
sms <sms@holumbus.org>
2008-05-10 23:17:45 +0200

    Documentation update & simplifications
    Added some lines of inline documentation and simplified the transformation from relative to absolute uris. The identification of references to other documents ist now configurable
    
    darcs-hash:20080510211745-ff21e-93d22609fbe7a6f39d188963ac387979c4c5547d.gz

--- source/Holumbus/Build/Crawl.hs
+++ source/Holumbus/Build/Crawl.hs
@@ -26,9 +26,9 @@ module Holumbus.Build.Crawl
   -- * Crawling
   , crawl
   
-  -- * Whatevering
-  , tmpFile
-  , initialCS
+  -- * Initializing
+  , initialCrawlerState
+  
   )
 where
 
@@ -45,7 +45,7 @@ import           Holumbus.Index.Common
 import           Holumbus.Index.Documents
 import           Holumbus.Utility
 
-import           Text.XML.HXT.Arrow     -- import all stuff for parsing, validating, and transforming XML
+import           Text.XML.HXT.Arrow
 import           System.Time
 
 
@@ -56,21 +56,22 @@ data CrawlerState b
     = CrawlerState
       { cs_toBeProcessed    :: S.Set URI
       , cs_wereProcessed    :: S.Set URI
-      , cs_unusedDocIds     :: [DocId]           -- probably unneeded
-      , cs_readAttributes   :: Attributes        -- passed to readDocument
-      , cs_crawlFilter      :: (URI -> Bool)     -- decides if a link will be followed
+      , cs_unusedDocIds     :: [DocId]        -- probably unneeded
+      , cs_readAttributes   :: Attributes     -- passed to readDocument
+      , cs_refXPaths        :: [String]       -- XPath expressions for references to other documents
+      , cs_fCrawlFilter     :: (URI -> Bool)  -- decides if a link will be followed
       , cs_docs             :: Documents b       
       , cs_tempPath         :: Maybe String     
       , cs_fGetCustom       :: Custom b
       }
       
--- | crawl a Web Page recursively
---   signature may change so that a doc table will be the computed result
+-- | The crawl function. MapReduce is used to scan the documents for references to other documents
+--   that have to be added to the 'Documents', too.
 crawl :: (Binary b) => Int -> Int -> CrawlerState b -> IO (Documents b)
 crawl traceLevel maxWorkers cs = 
-  if S.null ( cs_toBeProcessed cs )
-    then return (cs_docs cs)
-    else do
+  if S.null ( cs_toBeProcessed cs ) -- if no more documents have to be processed, 
+    then return (cs_docs cs)        -- the Documents are returned
+    else do                         -- otherwise, the crawling is continued
                  -- get the Docs that have to be processed and add docIds
                  -- for the MapReduce Computation
          d    <- return $ cs_toBeProcessed cs
@@ -80,61 +81,68 @@ crawl traceLevel maxWorkers cs =
                              , cs_wereProcessed = S.union d (cs_wereProcessed cs)
                              , cs_toBeProcessed = S.empty
                              }
-         mr   <- mapReduce 
-                    maxWorkers 
-                    (crawlDoc traceLevel 
-                              (cs_readAttributes cs) 
-                              (cs_tempPath cs)
-                              (cs_fGetCustom cs) ) 
-                    (processCrawlResults cs')
-                    d' 
-         
+
+         mr   <- mapReduce maxWorkers (crawlDoc traceLevel cs) (processCrawlResults cs') d' 
          cs'' <-  return $! snd (M.elemAt 0 mr)                    
          
          crawl traceLevel maxWorkers cs''
-    
+
+
+
+
+-- | The REDUCE function for the crawling MapReduce computation. The 'Documents' and their contained
+--   links are used to change the 'CrawlerState'. New documents are added to the list of 
+--   unprocessed docs and the data of the already crawled documents are added to the 'Documents'. 
 processCrawlResults :: (Binary b) => 
-                       CrawlerState b
-                    -> Int 
-                    -> [(Maybe (Document b), [URI])] 
+                       CrawlerState b                 -- ^ state before last MapReduce computation
+                    -> Int                            -- ^ artificial key, only to fit MapReduce
+                    -> [(Maybe (Document b), [URI])]  -- ^ data p in the crawl phase
                     -> IO (Maybe (CrawlerState b))
 processCrawlResults cs _ l =
     let
-      newDocs        = S.unions (map S.fromList (map refs l))
-      refs (_, uris) = filter (cs_crawlFilter cs) uris 
+        -- Concatenate reference lists of all documents and transform them into a set to eliminiate
+        -- multiple occurences of one 'Document'
+      newDocs        = S.unions (map S.fromList (map refs l))  
+        -- filter new references with the crawl filter
+      refs (_, uris) = filter (cs_fCrawlFilter cs) uris       
+        -- take only documents that could be crawled without errors
       processedDocs  = catMaybes (map fst l)
     in      
       return $! Just 
-        cs { cs_toBeProcessed = S.union
+        cs { cs_toBeProcessed = S.union                 -- add new documents to the todo list
                                   (cs_toBeProcessed cs)
                                   (S.difference newDocs (cs_wereProcessed cs))
-           , cs_docs          = theFold processedDocs
+           , cs_docs          = theFold processedDocs   -- insert crawled documents into 'Documents'
            } 
      where
-     theFold processedDocs
-       = foldl' (\d r -> snd (insertDoc d r)) 
-                             (cs_docs cs)
-                              processedDocs
+     theFold processedDocs = foldl' (\d r -> snd (insertDoc d r)) (cs_docs cs) processedDocs
+
+
+
+
 
 -- | Wrapper function for the "real" crawlDoc functions. The current time is
 --   traced (to identify documents that take a lot of time to be processed while
 --   testing) and the crawlDoc'-arrow is run
 crawlDoc :: (Binary b) => 
             Int 
-         -> Attributes 
-         -> Maybe String 
-         -> Custom b 
+         -> CrawlerState b
          -> DocId 
          -> String 
-         -> IO [(Int, (Maybe (Document b), [String]))]
-crawlDoc traceLevel attrs tmpPath getCustom docId theUri 
-  = do
+         -> IO [(Int, (Maybe (Document b), [URI]))]
+crawlDoc traceLevel cs docId theUri 
+  = let attrs     = cs_readAttributes cs 
+        tmpPath   = cs_tempPath cs
+        refXPaths = cs_refXPaths cs
+        getCustom = cs_fGetCustom cs 
+    in
+    do
     clt <- getClockTime               -- get Clock Time for debugging
     cat <- toCalendarTime clt         -- and convert it to "real" date and time
     runX (     setTraceLevel traceLevel
            >>> traceMsg 1 (calendarTimeToString cat)  
            >>> (     constA 42        -- this is needed to fit the MapReduce abstraction
-                 &&& crawlDoc' attrs tmpPath getCustom (docId, theUri)
+                 &&& crawlDoc' attrs tmpPath refXPaths getCustom (docId, theUri)
                )   
           ) 
 
@@ -143,40 +151,38 @@ crawlDoc traceLevel attrs tmpPath getCustom docId theUri
 crawlDoc' :: (Binary b) =>
      Attributes    -- ^ options for readDocument
   -> Maybe String  -- ^ path for serialized tempfiles
+  -> [String]
   -> Custom b
   -> (DocId, String)   -- ^ DocId, URI
-  -> IOSArrow c (Maybe (Document b), [String])
-crawlDoc' attrs tmpPath getCustom (docId, theUri) =
+  -> IOSArrow c (Maybe (Document b), [URI])
+crawlDoc' attrs tmpPath refXPaths getCustom (docId, theUri) =
         traceMsg 1 ("  crawling document: " ++ show docId ++ " -> " ++ show theUri )
     >>> readDocument attrs theUri
     >>> (
-          documentStatusOk `guards`
-          (
-                ( 
-                  (writeDocument [(a_indent, "1")] ((fromMaybe "" tmpPath) ++ (tmpFile docId theUri)))
+          documentStatusOk `guards`   -- make sure that the document could be accessed
+                -- if it is set in the crawler options, write a temporary copy of the document to
+                -- the hard disk
+          (     ( writeDocument [(a_indent, "1")] ((fromJust tmpPath) ++ (tmpFile docId theUri))
                   `whenP`
-                   (const (isJust tmpPath )) 
+                  const (isJust tmpPath ) 
                 ) 
-             >>> 
-                (   
---                  (constA Nothing)
-                  getDocument theUri getCustom 
-                  &&&
-                  (getRefs $< computeDocBase >>> strictA)
-                ) 
+                -- compute a pair of Document b (Holumbus datatype) and a list of contained links
+            >>> (     getDocument theUri getCustom
+                  &&& ( getRefs refXPaths >>> strictA )
+                )
           )
-          `orElse` ( 
-                clearErrStatus
+          `orElse` (                  -- if an error occurs with the current document, the global 
+                clearErrStatus        -- error status has to be reset, else the crawler would stop
             >>> traceMsg 0 (  "something went wrong with doc: " ++ theUri)    
-            >>> constA (Nothing, [])
-          )
+            >>> constA (Nothing, [])  -- Nothing indicates the error, the empty list shows
+          )                           -- that - caused by the error - no new links were found
         )
 
 -- | extract the Title of a Document (for web pages the <title> tag) and combine
 --   it with the document uri
 getDocument :: (Binary b) => 
-               URI 
-            -> Custom b
+               URI        -- ^ URI of the 'Document' 
+            -> Custom b   -- ^ Function to extract the custom Data from the 'Document'
             -> IOSArrow XmlTree (Maybe (Document b))
 getDocument theUri getCustom 
   = mkDoc $<<< (     getTitle
@@ -189,71 +195,29 @@ getDocument theUri getCustom
       =     getXPathTrees "/html/head/title/text()"
         >>> getText     
              
--- | Extract all Hyperlinks from a XmlTree
--- <br/>TODO: process frames and iframes
--- <br/>TODO: beautify & generalize protocol handling
--- <br/>TODO: beautify conversion from relative to absolute URLs
-getRefs :: (ArrowXml a) => String -> a XmlTree [String]
-getRefs base = listA $
---    ( 
-           (getXPathTrees "/descendant::a/attribute::href/child::text()"    >>> getText  )
---       &&& (getXPathTrees "/descendant::frame/attribute::src/child::text()"  >>> getText )
---    )              --       ^-- frames
---    >>> arr (\(a,b) -> a++b)
-    >>> isA supportedLink
-    >>> arr toAbsRef
-    where
-      supportedLink ref
-        = null rest
-        ||
-        proto `elem`  ["http", "https", "ftp", "file"]
-        where
-            (proto, rest) = span (/= ':') ref
+-- | Extract References to other documents from a XmlTree based on configured XPath expressions
+getRefs :: [String] -> IOSLA (XIOState s) XmlTree [URI]
+getRefs xpaths
+  = catA $ map getRefs' xpaths
+    where getRefs' xpath = listA $ getXPathTrees xpath >>> getText >>> mkAbsURI
 
-      toAbsRef ref
-        = removeFragment $ fromMaybe ref $ expandURIString ref base
-        where
-            removeFragment r
-              | "#" `isPrefixOf` path = reverse . tail $ path
-              | otherwise = r
-              where
-                path = dropWhile (/='#') . reverse $ r 
-
--- | compute the base URL of a complete document with root node
--- The complete document URL is stored within the attributes of the
--- root node. The base for accessing referenced docs is computed
--- by takin into account the "base" tag "href" value
--- <br/>Stolen from Uwe Schmidt
--- <br/>TODO: Does HXT provide a function for this task?
-computeDocBase  :: ArrowXml a => a XmlTree String
-computeDocBase
-    = ( ( ( this
-      /> hasName "html"
-      /> hasName "head"
-      /> hasName "base"
-      >>> getAttrValue "href"
-    )
-    &&&
-    getAttrValue "transfer-URI"
-  )
-  >>> expandURI
-      )
-      `orElse`
-      getAttrValue "transfer-URI"  
-      
 
 -- | create an initial CrawlerState from an IndexerConfig
-initialCS :: (Binary b) => IndexerConfig -> Custom b -> CrawlerState b
-initialCS cic getCustom
+initialCrawlerState :: (Binary b) => IndexerConfig -> Custom b -> CrawlerState b
+initialCrawlerState cic getCustom
   = CrawlerState
-      (S.fromList (ic_startPages cic))
-      S.empty
-      [1..]
-      (ic_readAttrs cic)
-      (ic_fCrawlFilter cic)
-      emptyDocuments
-      (ic_tmpPath cic)
-      getCustom           
+    { cs_toBeProcessed  = S.fromList (ic_startPages cic)
+    , cs_wereProcessed  = S.empty
+    , cs_unusedDocIds   = [1..]
+    , cs_readAttributes = ic_readAttrs cic
+    , cs_refXPaths      = ["//a/@href/text()", "//frame/@src/text()", "//iframe/@src/text()"]
+    , cs_fCrawlFilter   = ic_fCrawlFilter cic
+    , cs_docs           = emptyDocuments
+    , cs_tempPath       = ic_tmpPath cic
+    , cs_fGetCustom     = getCustom
+    }
+    
+          
           
         
 
