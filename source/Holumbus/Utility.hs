1c937a093011d84c249f5a5849f9f44003ae8aea
sms <sms@holumbus.org>
2008-05-10 23:21:40 +0200

    Moved some helper files to Holumbus.Utility
    Functions that are often needed for crawling and indexing tasks were moved to the Utility module to be available for library users.
    
    darcs-hash:20080510212140-ff21e-9bc8e796d6e6042a564fc901cd68070e0744ea9a.gz

--- source/Holumbus/Utility.hs
+++ source/Holumbus/Utility.hs
@@ -28,7 +28,7 @@ import qualified Data.IntMap as IM
 import qualified Data.List as L
 
 import           Data.Maybe
-
+import           Text.XML.HXT.Arrow
 import           Text.Regex
 
 -- | Split a string into seperate strings at a specific character sequence.
@@ -57,27 +57,81 @@ escape (c:cs)
       then c : escape cs
       else '%' : showHex (fromEnum c) "" ++ escape cs
       
--- | Computes a filename for a local temporary file.
---   Since filename computation might depend on the DocId it is also submitted
---   as a parameter
+{- | 
+     Computes a filename for a local temporary file.
+     Since filename computation might depend on the DocId it is also submitted
+     as a parameter
+-}
 tmpFile :: DocId -> URI -> String
 tmpFile _ u = escape u
 
--- | Helper function to replace original URIs by the corresponding pathes for 
---   the locally dumped files
---   <br/>TODO: Move this to a module
+{- | 
+     Helper function to replace original URIs by the corresponding pathes for 
+     the locally dumped files
+-}     
 tmpDocs :: String -> DOC.Documents a -> DOC.Documents a
 tmpDocs tmpPath =  
     DOC.fromMap 
   . (IM.mapWithKey (\docId doc -> Document (title doc) (tmpPath ++ (tmpFile docId (uri doc))) Nothing))
   . DOC.toMap         
       
--- | Helper function to create Crawl filters based on regular expressions.
---   A excluding regular expression is always stronger than a including one.
-mkCrawlFilter :: [String] -> [String] -> (URI -> Bool)
-mkCrawlFilter as ds theUri = isAllowed && (not isForbidden ) 
+
+{- | Create Crawl filters based on regular expressions. The first Parameter defines the default 
+     value if none of the supplied rules matches. The rule list is computed from the first element
+     to the last. The first rule that matches the URI is applied. 
+     
+     example:
+     
+     > crawlFilter False [ ("/a/b/z", True )
+     >                   , ("/a/b"  , False)
+     >                   , ("/a"    , True )
+
+     The default value for the filter is False like it will be in most cases unless you are trying
+     to use Holumbus to build a google replacement. If you read the rules from bottom to top, all
+     documents in "/a" will be included (which should be a single domain or ip address or maybe a
+     set of these). The second rule disables the directory "/a/b" but with the first rule, the 
+     subdirectory z is included again and "/a/b/a" to "/a/b/y" are excluded. Even though this could
+     also be done with the 'simpleCrawlFilter', this saves you a lot of unnecessary code.
+-} 
+crawlFilter :: Bool -> [(String, Bool)] -> (URI -> Bool)
+crawlFilter theDefault [] _ = theDefault
+crawlFilter theDefault ((expr, b):expressions) theUri = 
+  if isJust $ matchRegex (mkRegex expr) theUri then b else crawlFilter theDefault expressions theUri
+
+-- | Create Crawl filters based on regular expressions. The first list defines 
+--   regular expression of URIs that have to be crawled. Any new URI must match at least one of 
+--   these regular expressions to be crawled. The second list consists of regular expressions for
+--   pages that must not be crawled. This can be used to limit the set of documents defined by 
+--   the including expressions. 
+simpleCrawlFilter :: [String] -> [String] -> (URI -> Bool)
+simpleCrawlFilter as ds theUri = isAllowed && (not isForbidden ) 
          where
-         isAllowed   = foldl (&&) True  (map (doesMatch theUri) as)
-         isForbidden = foldl (||) False (map (doesMatch theUri) ds)
-         doesMatch u a = isJust $ matchRegex (mkRegex $ a) u      
+         isAllowed   = foldl (&&) True  (map (matches theUri) as)
+         isForbidden = foldl (||) False (map (matches theUri) ds)
+         matches u a = isJust $ matchRegex (mkRegex a) u      
       
+-- | some standard options for the readDocument function
+standardReadDocumentAttributes :: [(String, String)]
+standardReadDocumentAttributes = []
+  ++ [ (a_parse_html, v_1)]
+  ++ [ (a_issue_warnings, v_0)]
+  ++ [ (a_tagsoup, v_1) ]
+  ++ [ (a_use_curl, v_1)]
+  ++ [ (a_options_curl, "-L")] --"--user-agent HolumBot/0.1 --location")]     
+  ++ [ (a_encoding, isoLatin1)]      
+  
+
+
+
+-- | Comput the base of a webpage
+--   stolen from Uwe Schmidt, http://www.haskell.org/haskellwiki/HXT  
+computeDocBase  :: IOStateArrow s XmlTree String
+computeDocBase
+    = (     (     xshow (getXPathTrees "/html/head/base@href")
+              &&& getBaseURI
+            )
+        >>> expandURI
+      )
+      `orElse` getBaseURI
+
+  
