8b660017b301f64a2357db6163c5a5a4a718d5f5
sms <sms@holumbus.org>
2008-06-03 15:24:53 +0200

    Updated FH Wedel Indexer
    The Index is now built as a split index and then merged together to save memory
    
    darcs-hash:20080603132453-ff21e-59af8fd4960f43421e0f1262fb18c65e721fb703.gz

--- examples/fh-wedel/source/FHW/Indexer.hs
+++ examples/fh-wedel/source/FHW/Indexer.hs
@@ -1,16 +1,18 @@
 module Main
 where
-
-import Text.XML.HXT.Arrow
+import           Control.Monad
+import           Text.XML.HXT.Arrow
 import           Holumbus.Build.Crawl
 import           Holumbus.Build.Index
 import           Holumbus.Build.Tokenize
 import           Holumbus.Index.Common
+import           Holumbus.Index.Documents (Documents)
 import qualified Holumbus.Index.Documents as DOC
 import           Holumbus.Index.Inverted(emptyInverted)
 import           Holumbus.Utility
 
 import qualified Data.IntMap as IM
+import qualified Data.Set    as S
 import Data.Maybe
 
 main :: IO ()
@@ -19,8 +21,14 @@ main
     traceLevel     <- return 0
     workerThreads  <- return 7
     idxConfig      <- return ic_fhw
-    crawlState     <- return (initialCS idxConfig customFunction)
-
+    crawlState     <- return (initialCrawlerState idxConfig customFunction)
+    
+    {- runX (traceMsg 0 (" loading documents ... " ))
+    theDocs <- loadFromBinFile ( (ic_idxPath idxConfig) ++ "-docs.bin") :: IO (Documents Int)
+    crawlState     <- return $ fromDocuments crawlState theDocs
+    -}
+    
+    maxDocs        <- return 250
     -- ---------------------------------------------------------------------------------------------
     -- CRAWLING
     -- ---------------------------------------------------------------------------------------------
@@ -36,51 +44,232 @@ main
     runX (traceMsg 0 (" indexing  ----------------------------- " ))
 
     localDocs <- return $ tmpDocs (fromMaybe "/tmp" (ic_tmpPath idxConfig)) docs
-    idx    <- buildIndex workerThreads
+
+    pathes    <- buildSplitIndex 
+                         workerThreads
                          traceLevel
                          (map (\(i,d) -> (i, uri d)) (IM.toList $ DOC.toMap localDocs))
                          idxConfig
-                         emptyInverted 
-
-    writeToXmlFile ( (ic_idxPath idxConfig) ++ "-index.xml") idx
-    writeToBinFile ( (ic_idxPath idxConfig) ++ "-index.bin") idx
-
-    return()
+                         emptyInverted
+                         True
+                         maxDocs  
+                         
+    idx       <- foldM mergeIndexes' emptyInverted pathes
     
+    writeToXmlFile ( (ic_idxPath idxConfig) ++ "-index.xml") idx
+    writeToBinFile ( (ic_idxPath idxConfig) ++ "-index.bin") idx    
+  
+    return()
+    where
+      mergeIndexes' i1 f = do
+                           i2 <- loadFromBinFile (f  ++ "-index.bin")
+                           return $ mergeIndexes i1 i2
+                           
+fromDocuments :: CrawlerState a -> Documents a -> CrawlerState a
+fromDocuments cs ds = cs { cs_toBeProcessed = S.fromList ( map (uri . snd) ( IM.toList $ DOC.toMap ds )) }
+     
 customFunction :: ArrowXml a => a XmlTree (Maybe Int)
 customFunction = constA Nothing    
+
+ic_test :: IndexerConfig
+ic_test = ic_fhw {ic_startPages = ["http://trac.schlatt.com/test/index.html"], ic_fCrawlFilter = const True}
 
 ic_fhw :: IndexerConfig
 ic_fhw 
   = IndexerConfig
-    { ic_startPages     = [ "http://www.fh-wedel.de"
+    { ic_startPages     = [ "http://www.fh-wedel.de/"
                           , "http://www.fh-wedel.de/sonstiges/sitemap/"
+                          , "http://www.fh-wedel.de/wir-ueber-uns/mitarbeiter-innen/?no_cache=1"
                           ]
     , ic_tmpPath        = Just "/tmp/"
-    , ic_idxPath        = "/home/sms/indexes/sitemap"
+    , ic_idxPath        = "/home/sms/indexes/fhw"
     , ic_contextConfigs = ccs_fhw
-    , ic_readAttrs      = stdOpts4Reading
-    , ic_fCrawlFilter   = mkCrawlFilter [ "http://(www)?\\.fh-wedel\\.de" ]                 -- allow
-                                        [ "tx_fhwunternehmensforum_pi3"                     -- deny
+    , ic_readAttrs      = standardReadDocumentAttributes
+    , ic_fCrawlFilter   = simpleCrawlFilter -- [ "^http://www\\.fh-wedel\\.de"] -- 
+                                            ["^http://[a-z]*\\.?fh-wedel\\.de" ]           -- allow
+                                        (["tx_fhwunternehmensforum_pi3"                     -- deny
+                                        , "http://asta.fh-wedel.de"                -- slow
+                                        , "http://biblserv.fh-wedel.de"            -- slow
 --                                        , "/~", "/%7E", "http://www.fh-wedel.de/mitarbeiter/"
-                                        , "\\?L=0"
+                                        , "\\?L=0", "\\&L=0"
                                         , ".pdf$", ".jpg$", ".gif$", ".png$", ".tar.gz$"
                                         , ".ppt$", ".exe$", ".txt$", ".zip$", ".doc$"
                                         , ".dot$", ".png$", ".ps$", ".ps.gz$", ".nb$"
-                                        , ".swf$", ".JPG$", ".tex$"
+                                        , ".swf$", ".JPG$", ".tex$", ".rss$", ".mpg$"
+                                        , ".mp3$", ".java$", ".tgz$", ".svg", ".mdb$" 
+                                        , ".PDF$", ".xls$", ".dta$", ".lst$", ".rar"
                                         , "%7Edi", "/~di"
+                                        , "ws99/Ausarbeitung/mico/Beispiel"
                                         , "/rundgang/id=", "/vorlesungsplan/id="
                                         , "/vorlesungsplan/sem=", "/tv-infosystem/", "/~splan/"
-                                        ]
+                                        , "http://www\\.fh-wedel\\.de/index\\.php\\?eID=tx_cms_showpic"
+                                        , "http://www.fh-wedel.de/fileadmin/mitarbeiter/ne/CG/opengl_man"
+                                        , "http://www.fh-wedel.de/%7Esi/vorlesungen/c/beispiele"
+                                        , "http://www.fh-wedel.de/~si/vorlesungen/c/beispiele"
+                                        ] ++ list404)
     }
     
+    
+    
+list404 :: [String]
+list404 = 
+  [ "http://www.fh-wedel.de/work/medienlabor-der-fh-wedel/raeumlichkeiten/rechenzentrum/"
+  , "http://www.fh-wedel.de/typo3"
+  , "http://www.fh-wedel.de/~ue"
+  , "http://www.fh-wedel.de/~wk"
+  , "http://www.fh-wedel.de/work/medienlabor-der-fh-wedel/raeumlichkeiten/videostudio/"
+  , "http://www.fh-wedel.de/studierende/campus-infos/fh-stream/team.php"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/research/index.html"
+  , "http://www.fh-wedel.de/studierende/campus-infos/fh-stream/streams.php"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/vrlab/index.html"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/refs/index.html"
+  , "http://www.fh-wedel.de/studierende/campus-infos/fh-stream/specials.php"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/virtual-reality-labor/index.html"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/index.html"
+  , "http://www.fh-wedel.de/studierende/campus-infos/fh-stream/kontakt.php"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/virtual-reality-labor/cobench/index.html"
+  , "http://www.fh-wedel.de/studierende/campus-infos/online-tutorials/vlc-usage.htm"
+  , "http://www.fh-wedel.de/studierende/campus-infos/fh-stream/index.php"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/courses/index.html"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/virtual-reality-labor/cave/index.html"
+  , "http://www.fh-wedel.de/studierende/campus-infos/online-tutorials/toc_index.htm"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/vrlab/cobench/index.html"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/news/index.html"
+  , "http://www.fh-wedel.de/wir-ueber-uns/labore-fe/informatik/contact/index.html"
+  , "http://www.fh-wedel.de/studierende/campus-infos/fh-stream/faq.php"
+  , "http://www.fh-wedel.de/internationaloffice/Deutsch/"
+  , "http://www.fh-wedel.de/%7Ege/Seminar.htm"
+  , "http://www.fh-wedel.de/HXmlToolbox/index.html"
+  , "http://www.fh-wedel.de/Literatur_08.html"
+  , "http://www.fh-wedel.de/Seminar_Algorithmische_Graphentheorie.html"
+  , "http://www.fh-wedel.de/Seminar_Kombinatorik.html"
+  , "http://www.fh-wedel.de/absolventen/members/absolventensuche.html"
+  , "http://www.fh-wedel.de/analysis-im-Netz.htm"
+  , "http://www.fh-wedel.de/bba/index.html"
+  , "http://www.fh-wedel.de/buecher.html"
+  , "http://www.fh-wedel.de/buero_n15.html"
+  , "http://www.fh-wedel.de/c/index.html"
+  , "http://www.fh-wedel.de/cg1/index.html"
+  , "http://www.fh-wedel.de/cg2/index.html"
+  , "http://www.fh-wedel.de/cprog.html"
+  , "http://www.fh-wedel.de/ctrl.html"
+  , "http://www.fh-wedel.de/dbprog.html"
+  , "http://www.fh-wedel.de/diplom.html"
+  , "http://www.fh-wedel.de/download.htm"
+  , "http://www.fh-wedel.de/downloads.html"
+  , "http://www.fh-wedel.de/dozent.html"
+  , "http://www.fh-wedel.de/faelle.html"
+  , "http://www.fh-wedel.de/gesetze.html"
+  , "http://www.fh-wedel.de/gisela.html"
+  , "http://www.fh-wedel.de/handout.html"
+  , "http://www.fh-wedel.de/index.html"
+  , "http://www.fh-wedel.de/index/index.html"
+  , "http://www.fh-wedel.de/infos/mitarbeiter"
+  , "http://www.fh-wedel.de/infos/mitarbeiter/index.html"
+  , "http://www.fh-wedel.de/infos/sprechzeiten/"
+  , "http://www.fh-wedel.de/klausuren.html"
+  , "http://www.fh-wedel.de/~si/seminare/ws05/Ausarbeitung/7.concurrent/index.html"
+  , "http://www.fh-wedel.de/cis/semesterinfo/richtlinien/doku-richtl.html"
+  , "http://www.fh-wedel.de/~si/seminare/ws06/Ausarbeitung/04.UserModeLinux/index.html"
+  , "http://www.fh-wedel.de/cis/semesterinfo/studienordnung/index.html"
+  , "http://www.fh-wedel.de/~ge/Datenbanken.htm"
+  , "http://www.fh-wedel.de/~si/seminare/ws06/Ausarbeitung/14.Cocoon/cocoon0.htm"
+  , "http://www.fh-wedel.de/~si/seminare/ws00/Ausarbeitung/index.html"
+  , "http://www.fh-wedel.de/~si/seminare/ws01/Ausarbeitung/index.html"
+  , "http://www.fh-wedel.de/%7Efi/odb/index.html"
+  , "http://www.fh-wedel.de/~si/seminare/ws02/Ausarbeitung/index.html"
+  , "http://www.fh-wedel.de/%7Ekap/"
+  , "http://www.fh-wedel.de/~si/seminare/ws99/Ausarbeitung/index.html"
+  , "http://www.fh-wedel.de/%7Eps2/programmierstil/delphi_progstil.html"
+  , "http://www.fh-wedel.de/%7Eps2/programmierstil/inline_doku.html"
+  , "http://www.fh-wedel.de/%7Eps2/programmierstil/pascal_progstil.html"
+  , "http://www.fh-wedel.de/~mo/lectures/seminar-sose03.html"
+  , "http://www.fh-wedel.de/~si/seminare/ws03/Ausarbeitung/index.html"
+  , "http://www.fh-wedel.de/~si/HXmlToolbox/hdoc/index.htm"
+  , "http://www.fh-wedel.de/archiv/iw/Lehrveranstaltungen/WS2006/GdT.html"
+  , "http://www.fh-wedel.de/~si/seminare/ws04/Ausarbeitung/index.html"
+  , "http://www.fh-wedel.de/~si/seminare/ws04/Termine/www.wikipedia.org"
+  , "http://www.fh-wedel.de/~ki/ia/sem3/seminar1/code.htm"
+  , "http://www.fh-wedel.de/klausuren/Compilerbau.html"
+  , "http://www.fh-wedel.de/klausuren/CundOOP.html"
+  , "http://www.fh-wedel.de/klausuren/SD.html"
+  , "http://www.fh-wedel.de/klausuren/Unix.html"
+  , "http://www.fh-wedel.de/klausuren/index.html"
+  , "http://www.fh-wedel.de/klr2.html"
+  , "http://www.fh-wedel.de/kontakt.html"
+  , "http://www.fh-wedel.de/lebenslauf.html"
+  , "http://www.fh-wedel.de/links.htm"
+  , "http://www.fh-wedel.de/links.html"
+  , "http://www.fh-wedel.de/material.html"
+  , "http://www.fh-wedel.de/mec_MInf.html"
+  , "http://www.fh-wedel.de/mitarbeiter/iw/lehrveranstaltungen-im-naechsten-semester/informatik-seminar-algorithmen/"
+  , "http://www.fh-wedel.de/oop/index.html"
+  , "http://www.fh-wedel.de/or.html"
+  , "http://www.fh-wedel.de/personal.html"
+  , "http://www.fh-wedel.de/phys1_TInf.html"
+  , "http://www.fh-wedel.de/phys1_WIng.html"
+  , "http://www.fh-wedel.de/planspiel.html"
+  , "http://www.fh-wedel.de/planspiel2.html"
+  , "http://www.fh-wedel.de/pmod.html"
+  , "http://www.fh-wedel.de/praktika/MultimediaProjekte/SVG/SVG_Tutorial_mi3794/index.htm"
+  , "http://www.fh-wedel.de/praktika/MultimediaProjekte/index.html"
+  , "http://www.fh-wedel.de/praktika/SoftwarePraktikum/index.html"
+  , "http://www.fh-wedel.de/projekt.html"
+  , "http://www.fh-wedel.de/projekte/aktuell.htm"
+  , "http://www.fh-wedel.de/pse.html"
+  , "http://www.fh-wedel.de/rechnernetze/nwslab.htm"
+  , "http://www.fh-wedel.de/seite2.htm"
+  , "http://www.fh-wedel.de/seminare.html"
+  , "http://www.fh-wedel.de/seminare/index.html"
+  , "http://www.fh-wedel.de/seminare/ss03/Termine/Themen.html"
+  , "http://www.fh-wedel.de/seminare/ss04/Termine/Themen.html"
+  , "http://www.fh-wedel.de/seminare/ws03/Termine/Themen.html"
+  , "http://www.fh-wedel.de/seminare/ws04/Termine/Themen.html"
+  , "http://www.fh-wedel.de/seminare/ws05/Termine/Themen.html"
+  , "http://www.fh-wedel.de/seminare/ws06/Termine/Themen.html"
+  , "http://www.fh-wedel.de/sprechstunde.htm"
+  , "http://www.fh-wedel.de/suche/"
+  , "http://www.fh-wedel.de/termine.htm"
+  , "http://www.fh-wedel.de/termine/Klausureinsicht.html"
+  , "http://www.fh-wedel.de/termine/Sprechstunde.html"
+  , "http://www.fh-wedel.de/termine/index.html"
+  , "http://www.fh-wedel.de/toc_index.htm"
+  , "http://www.fh-wedel.de/uebungen/hinweise/hinweise.html"
+  , "http://www.fh-wedel.de/uebungen/oop/aufgaben/aufg04.html"
+  , "http://www.fh-wedel.de/uebungen/oop/aufgaben/aufg05.html"
+  , "http://www.fh-wedel.de/uebungen/oop/index.html"
+  , "http://www.fh-wedel.de/uebungen/oop/richtlinien.html"
+  , "http://www.fh-wedel.de/uebungen/unix/index.html"
+  , "http://www.fh-wedel.de/veranstaltungen.htm"
+  , "http://www.fh-wedel.de/vl.html"
+  , "http://www.fh-wedel.de/vlc-usage.htm"
+  , "http://www.fh-wedel.de/vorlesungen/c/c.html"
+  , "http://www.fh-wedel.de/vorlesungen/cb/cb.html"
+  , "http://www.fh-wedel.de/vorlesungen/fp/fp.html"
+  , "http://www.fh-wedel.de/vorlesungen/index.html"
+  , "http://www.fh-wedel.de/vorlesungen/internet/Literatur/Buecher.html"
+  , "http://www.fh-wedel.de/vorlesungen/internet/internet.html"
+  , "http://www.fh-wedel.de/vorlesungen/java/Literatur/Buecher.html"
+  , "http://www.fh-wedel.de/vorlesungen/java/java.html"
+  , "http://www.fh-wedel.de/vorlesungen/softwaredesign/Literatur/Buecher.html"
+  , "http://www.fh-wedel.de/vorlesungen/softwaredesign/design.html"
+  , "http://www.fh-wedel.de/vr/index.html"
+  , "http://www.fh-wedel.de/xml2html.2/xml2html/xml/index.xml"
+  , "http://www.fh-wedel.de/zettelkasten/etc/archiv.html"
+  , "http://www.fh-wedel.de/zettelkasten/etc/index.html"
+  , "http://www.fh-wedel.de/zettelkasten/etc/schwob.html"
+  , "http://www.fh-wedel.de/zettelkasten/fortune/fhspruch.html"
+  , "http://www.fh-wedel.de/zettelkasten/fortune/index.html"
+  , "http://www.fh-wedel.de/zettelkasten/fortune/index.html?KEY=1147779735"
+  ]  
+  
 -- | The list of context configurations for the Fh Wedel pages
 ccs_fhw :: [ContextConfig]
 ccs_fhw = []
       ++ [cc_title]
       ++ [cc_meta]
       ++ [cc_content]
-      ++ [cc_raw]
+--      ++ [cc_raw]
 
     
 -- | Context for title-tags
@@ -92,6 +281,7 @@ cc_title
     , cc_XPath        = "/html/head/title"  
     , cc_fTokenize    = map (stripWith (=='.')) . (parseWords isWordChar)
     , cc_fIsStopWord  = const False -- (\s -> length s < 2)
+    , cc_addToCache   = True
     }
     
 -- | Context for meta information. Description and keywords will be indexed
@@ -103,6 +293,7 @@ cc_meta
     , cc_XPath        = "/html/head/meta[@name='description' or @name='keywords']/@content"  
     , cc_fTokenize    = map (stripWith (=='.')) . (parseWords isWordChar)
     , cc_fIsStopWord  = (\s -> length s < 2)
+    , cc_addToCache   = True
     }    
     
 -- | Context for normal page content. This indexes everything that is inside a div element with id
@@ -116,6 +307,7 @@ cc_content
     , cc_XPath        = "//div[@id='col2_content']"  
     , cc_fTokenize    = map (stripWith (=='.')) . (parseWords isWordChar)
     , cc_fIsStopWord  = (\s -> length s < 2)
+    , cc_addToCache   = True
     }
 
 -- | Raw context. This indexes all the text that is listed on the page without check if it is in
@@ -128,15 +320,7 @@ cc_raw
     , cc_XPath        = "//body"  
     , cc_fTokenize    = map (stripWith (=='.')) . (parseWords isWordChar)
     , cc_fIsStopWord  = (\s -> length s < 2)
+    , cc_addToCache   = True
     }
     
         
-    
--- | some standard options for the readDocument function
-stdOpts4Reading :: [(String, String)]
-stdOpts4Reading = []
-  ++ [ (a_parse_html, v_1)]
-  ++ [ (a_issue_warnings, v_0)]
-  ++ [ (a_tagsoup, v_1) ]
-  ++ [ (a_use_curl, v_1)]
-  ++ [ (a_options_curl, "-L")] --"--user-agent HolumBot/0.1 --location")]     
