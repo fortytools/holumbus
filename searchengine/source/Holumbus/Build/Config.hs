0ea9ccc3333282f1698782cc07bbab1e169d1975
uwe <uwe@fh-wedel.de>
2008-06-30 21:27:36 +0200

    standardReadDocumentAttributes simplified
    darcs-hash:20080630192736-be05e-2171334e2ff7eab18f63a667b55c04a1d1d74f90.gz

--- searchengine/source/Holumbus/Build/Config.hs
+++ searchengine/source/Holumbus/Build/Config.hs
@@ -261,14 +261,13 @@ simpleCrawlFilter as ds theUri = isAllowed && (not isForbidden )
       
 -- | some standard options for the readDocument function
 standardReadDocumentAttributes :: [(String, String)]
-standardReadDocumentAttributes = []
-  ++ [ (a_parse_html,        v_1)]
-  ++ [ (a_issue_warnings,    v_0)]
-  ++ [ (a_remove_whitespace, v_1)]
-  ++ [ (a_tagsoup,           v_1)]
-  ++ [ (a_use_curl,          v_1)]
-  ++ [ (a_options_curl,      "--user-agent HolumBot/0.1@http://holumbus.fh-wedel.de --location")]     
-  ++ [ (a_encoding,          isoLatin1)]      
-
-
-
+standardReadDocumentAttributes
+    = [ (a_parse_html,			v_1)
+      , (a_encoding,			isoLatin1)
+      , (a_issue_warnings,		v_0)
+      , (a_remove_whitespace,		v_1)
+      , (a_tagsoup,			v_1)
+      , (a_ignore_none_xml_contents,	v_1)
+      , (a_use_curl,			v_1)	-- obsolete since hxt-8.1
+      , ("curl--user-agent",  		"HolumBot/0.1@http://holumbus.fh-wedel.de --location")
+      ]
