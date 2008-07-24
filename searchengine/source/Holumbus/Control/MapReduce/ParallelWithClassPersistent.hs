c8a999c4b2f853e495cbc2ab1ecb93a8745c0f7d
sms <sms@holumbus.org>
2008-07-24 16:21:55 +0200

    Changed decoding
    Values that are read from the SQLite database are now decoded after the connection mvar has been released.
    
    darcs-hash:20080724142155-ff21e-ca38aa041b3e82efc7d8ca0b27f0fbdfe6bc3f48.gz

--- searchengine/source/Holumbus/Control/MapReduce/ParallelWithClassPersistent.hs
+++ searchengine/source/Holumbus/Control/MapReduce/ParallelWithClassPersistent.hs
@@ -24,8 +24,7 @@ module Holumbus.Control.MapReduce.ParallelWithClassPersistent
   ) 
 where
 
-import Text.XML.HXT.Arrow
-
+import           Data.List (sort)
 import           Data.Map (Map,empty) -- ,mapWithKey,filterWithKey)
 import qualified Data.Map    as M
 import           Data.Maybe (isJust, fromJust)
@@ -43,63 +42,71 @@ import           Holumbus.Utility
 import           Database.HDBC
 import           Database.HDBC.Sqlite3
 
+import           System.Time
+import           System.Timeout
+
 -- ----------------------------------------------------------------------------
 
 -- | MapReduce Computations
-mapReduce :: (Ord k2 , Binary v2, MapReducible mr k2 v2) =>
+mapReduce :: (Show k1, Ord k2 , Show k2, Binary v2, MapReducible mr k2 v2) =>
                 Int                             -- ^ No. of Threads 
+             -> Int                             -- ^ timeout for mAP-tasks 
              -> FilePath                        -- ^ Path for SQLite database
              -> mr                              -- ^ initial value for the result
              -> (k1 ->  v1  -> IO [(k2, v2)])   -- ^ Map function
              -> [(k1, v1)]                      -- ^ input data 
              -> IO (mr)  
-mapReduce maxWorkers sqlitePath mr mapFunction input
+mapReduce maxWorkers mapTimeout sqlitePath mr mapFunction input
   = do
     
-    conn <- createConnection sqlitePath                        -- create Sqlite Connection
-    
+    conn <- createConnection sqlitePath                                  -- create Sqlite Connection
                                                                 
-    runX (traceMsg 0 ("                    mapPerKey " ))      
-    mapped <- parallelMap conn maxWorkers  mapFunction input   -- parallel mAP phase
+    putStrLn ("-- (0)                    mapPerKey " )
+    mapped <- parallelMap conn maxWorkers mapTimeout mapFunction input   -- parallel mAP phase
     
-    -- reduce phase
-    runX (traceMsg 0 ("                    reduceByKey "))     
-    reducePerKey conn maxWorkers mr mapped                     -- parallel rEDUCE phase
+    putStrLn ("-- (0)                    reduceByKey ")
+    reducePerKey conn maxWorkers mr mapped                               -- parallel rEDUCE phase
     
 -- ----------------------------------------------------------------------------
 
 -- | Executes the map phase of a MapReduce computation as a parallel computation.
-parallelMap :: (Ord k2, Binary v2) => 
+parallelMap :: (Show k1, Ord k2, Binary v2) => 
      Connection                    -- ^ SQLite database connection 
   -> Int                           -- ^ maximum number of threads
+  -> Int                           -- ^ timeout for mAP-tasks 
   -> (k1 ->  v1  -> IO [(k2, v2)]) -- ^ mAP function
   -> [(k1, v1)]                    -- ^ input data
   -> IO (Map k2 Int)               -- ^ dictionary mapping keys to database identifiers
-parallelMap conn maxWorkers  mapFunction inputData
+parallelMap conn maxWorkers mapTimeout mapFunction inputData
   = if (length inputData > 0)   -- start recursive function only if input data is supplied
       then do
-           chan <- newChan      -- open communication channel and call recursive helper function
-           parallelMap' chan conn 0 maxWorkers mapFunction 1 inputData M.empty
+           chan    <- newChan      -- open communication channel and call recursive helper function
+           logChan <- newChan
+           forkIO (showLogs logChan)
+           parallelMap' logChan chan conn 0 maxWorkers mapTimeout mapFunction 1 inputData M.empty
       else return M.empty
 
 -- | Recursive helper function for the parallel mAP phase
-parallelMap' :: (Ord k2, Binary v2) => 
-     Chan [(k2, v2)]               -- ^ communication channel for main-thread and workers  
+parallelMap' :: (Show k1, Ord k2, Binary v2) => 
+     Chan String 
+  -> Chan [(k2, v2)]               -- ^ communication channel for main-thread and workers  
   -> Connection                    -- ^ SQLite database connection
   -> Int                           -- ^ active worker-thread count
   -> Int                           -- ^ maximum number of workers 
+  -> Int                           -- ^ timeout for mAP-tasks 
   -> (k1 ->  v1  -> IO [(k2, v2)]) -- ^ mAP function
   -> Int                           -- ^ next database id to use
   -> [(k1, v1)]                    -- ^ input data 
   -> Map k2 Int                    -- ^ accumulator for the result 
   -> IO (Map k2 Int)               -- ^ dictionary mapping keys to database identifiers
-parallelMap' chan conn activeWorkers maxWorkers mapFunction nextId inputData acc
+parallelMap' logChan chan conn activeWorkers maxWorkers mapTimeout mapFunction nextId inputData acc
   = do
     if (activeWorkers < maxWorkers) && ((length inputData) > 0)    -- spawn new workers ?     
       then do
-           runMapTask chan mapFunction (head inputData)            -- start a new worker
-           parallelMap' chan conn (activeWorkers + 1) maxWorkers   -- recursive call
-                        mapFunction nextId (tail inputData) acc
+           runMapTask logChan chan mapTimeout mapFunction (head inputData) -- start a new worker
+           parallelMap' logChan chan conn (activeWorkers + 1)      -- recursive call
+                        maxWorkers mapTimeout mapFunction nextId 
+                        (tail inputData) acc
       else if (activeWorkers == 0) -- && (length inputData == 0)   -- mAP phase finished ?
              then return acc                                       -- if yes return result
              else do                                              
@@ -111,7 +118,8 @@ parallelMap' chan conn activeWorkers maxWorkers mapFunction nextId inputData acc
                   (n, a) <- foldM processValue (nextId', acc') res -- process results from 1st worker
                   e     <- isEmptyChan chan                        -- more results in chan?
                   if e
-                    then parallelMap' chan conn (workers - 1) maxWorkers mapFunction n inputData (a)
+                    then parallelMap' logChan chan conn (workers - 1) maxWorkers 
+                                      mapTimeout mapFunction n inputData (a)
                     else readValues (workers -1) n a
   processValue :: (Ord k2, Binary v2) => (Int, Map k2 Int) -> (k2, v2) -> IO (Int, Map k2 Int)
   processValue (next, m) (k2, v2) = 
@@ -121,21 +129,34 @@ parallelMap' chan conn activeWorkers maxWorkers mapFunction nextId inputData acc
     return $ (if next == k2Id then next + 1 else next, M.insert k2 k2Id m) 
 
 
-runMapTask :: Chan [(k2, v2)] -> (k1 ->  v1  -> IO [(k2, v2)]) -> (k1, v1) -> IO ()
-runMapTask chan mapFunction (k1, v1)                                  -- start new Thread
-  = do forkIO ( do                                                    
-                res <- catch (mapFunction k1 v1) (\_ -> return $ [])  -- apply mAP
-                writeChan chan res                                    -- write result to channel  
-                return ()
-              )
-       return ()
+runMapTask :: Show k1 => Chan String -> Chan [(k2, v2)] -> Int -> (k1 ->  v1  -> IO [(k2, v2)]) -> (k1, v1) -> IO ()
+runMapTask logChan chan mapTimeout mapFunction (k1, v1)                                  -- start new Thread
+  = forkIO
+     ( do 
+       writeChan logChan ("--     mAP:    processing " ++ show k1)
+       res <- catch ( do
+                      r <- timeout mapTimeout (mapFunction k1 v1) -- apply mAP
+                      if isJust r
+                        then return $ fromJust r
+                        else return $ []
+                    )   
+                    (\_ -> return $ [])                           -- make sure, a result is returned
+       writeChan chan res                                         -- write result to channel  
+     ) >> return ()
 
 -- ----------------------------------------------------------------------------
 
+
+showLogs :: Chan (String) -> IO ()
+showLogs logChan = do
+                   s <- readChan logChan
+--                   putStrLn s
+                   showLogs logChan
+                   
 reducePerKey
   , reducePerKeyParallel
 --  , reducePerKeySequential 
-  :: (MapReducible mr k2 v2, Binary v2) => 
+  :: (MapReducible mr k2 v2, Show k2, Binary v2) => 
   Connection -> Int -> mr -> Map k2 Int -> IO (mr)
 reducePerKey = reducePerKeyParallel
 
@@ -159,34 +180,53 @@ reducePerKeySequential conn _ initialMR m
 
 
 reducePerKeyParallel conn maxWorkers initialMR m = 
-  let input   = partitionList maxWorkers (M.toList m)    -- partition input data
-      workers = length input                             -- get real worker count
-  in 
+  let input   = partitionListByCount maxWorkers (sort $ M.toList m)    -- partition input data
+      workers = length input                                           -- get real worker count
+  in
   do
-  chan <- newChan
+--  putStrLn ("workercount: " ++ show workers)
+  resChan     <- newChan
+  logChan  <- newChan
+  forkIO (showLogs logChan)
   connMVar <- newMVar conn
-  mapM (runReduceTask connMVar chan initialMR) input
-  foldM (\mr _ -> do
-                  res <- readChan chan
-                  if isJust res then return (mergeMR mr (fromJust res)) else return mr
-        ) initialMR [1..workers]
-  
+--  mapM (runReduceTask logChan connMVar chan initialMR) (zip [1..] input)
+  spawnThreads logChan connMVar resChan (zip [1..] input)     -- spawn worker threads
+  foldM (\mr (c,a) -> do
+                      res <- readChan resChan
+                      clt <- getClockTime
+                      cat <- toCalendarTime clt
+                      writeChan logChan ("--  " ++ (calendarTimeToString cat) ++ " processing result " ++ show c ++ " / " ++ show a)
+                      if isJust res then mergeMR mr (fromJust res) else return mr
+        ) initialMR ( zip [1..workers] (repeat workers) )
+  where 
+    spawnThreads _ _ _ [] = return ()         -- no more threads to spawn
+    spawnThreads logChan connMVar resChan l  = do    -- spawn new thread and make recursive call
+                                      runReduceTask logChan connMVar resChan initialMR (head l)
+                                      spawnThreads logChan connMVar resChan (tail l)
 
 
-runReduceTask :: (MapReducible mr k2 v2, Binary v2) => MVar Connection -> Chan (Maybe mr) ->  mr -> [(k2, Int)] -> IO ()
-runReduceTask connMVar chan initialMR input                                  -- start new Thread
+
+    
+
+
+
+runReduceTask :: (MapReducible mr k2 v2, Show k2, Binary v2) => 
+                 Chan (String) -> MVar Connection -> Chan (Maybe mr) ->  mr -> (Int, [(k2, Int)]) -> IO ()
+runReduceTask logChan connMVar chan initialMR (threadId, input)
   = forkIO                                                                -- spawn new thread
     ( do                                                    
       res <- catch                                                        -- catch exception so the
              ( do                                                         -- MapReduce can continue
                r <- foldM (  \mr (k2, v2Id)                               -- if one worker fails
                            -> do
-                              conn <- takeMVar connMVar                   -- exclusive db access
-                              v2   <- retrieveValues conn v2Id            -- get v2s from db
-                              putMVar connMVar conn                       -- release db access
-                              val  <- reduceMR initialMR k2 v2            -- apply rEDUCE function
-                              if isJust val                               -- process result
-                                 then return (mergeMR mr (fromJust val))
+                              writeChan logChan ("--    rEDUCE: thread " ++ show threadId ++ " processing " ++ show k2)
+                              conn  <- takeMVar connMVar                   -- exclusive db access
+                              v2sraw <- retrieveValues' conn v2Id          -- get v2s from db
+                              putMVar connMVar conn                        -- release db access
+                              let v2s = map (\v -> decode $ BL.fromChunks [fromSql $ head v]) v2sraw
+                              val   <- reduceMR initialMR k2 v2s           -- apply rEDUCE function
+                              if isJust val                                -- process result
+                                 then mergeMR mr (fromJust val)
                                  else return mr
                           ) initialMR input
                return $ Just r
@@ -207,17 +247,28 @@ retrieveValues conn di =
   r <- quickQuery conn lookupQuery [toSql di] 
   mapM (\v -> return $ decode $ BL.fromChunks [fromSql $ head v]) r
   
-
+retrieveValues' :: Connection -> Int -> IO [[SqlValue]]
+retrieveValues' conn di =
+  do
+  clt <- getClockTime
+  r <- quickQuery conn lookupQuery [toSql di]  
+  clt' <- getClockTime
+  putStrLn $ show (diffClockTimes clt' clt)
+  return r
+  
 createConnection :: FilePath -> IO Connection
 createConnection f = do
                      conn <- connectSqlite3 f
                      t <- getTables conn
                      if  ("mr" `elem` t)
-                       then return conn -- error ("Holumbus.Control.MapReduce.ParallelWithClassPersistent: Table mr already exists in database " ++ f)
+                       then quickQuery conn deleteQuery [] >> commit conn >> return conn -- error ("Holumbus.Control.MapReduce.ParallelWithClassPersistent: Table mr already exists in database " ++ f)
                        else quickQuery conn createQuery [] >> commit conn >> return (conn)
 
 createQuery :: String
 createQuery = "CREATE TABLE mr (k2id INTEGER, v2 BLOB)"
+
+deleteQuery :: String
+deleteQuery = "DELETE FROM mr"
 
 insertQuery :: String
 insertQuery = "INSERT OR REPLACE INTO mr (k2id, v2) VALUES (?, ?)"
