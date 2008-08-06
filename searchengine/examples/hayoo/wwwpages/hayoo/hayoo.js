20eb5c5f4225b42b2ab0e045f47d7b4c8c4791d2
tbh <tbh@holumbus.org>
2008-08-06 22:19:16 +0200

    Adjusted Hayoo! JavaScript and OpenSearch file
    darcs-hash:20080806201916-67f31-89315cd45c3f70e26762449d05f3dee299532e24.gz

--- searchengine/examples/hayoo/wwwpages/hayoo/hayoo.js
+++ searchengine/examples/hayoo/wwwpages/hayoo/hayoo.js
@@ -11,90 +11,93 @@
 */
 
 prevInput = "";
-lastLocation = window.location.href;
+lastLocation = window.location.hash;
 
-Event.observe(window, 'load', checkForQuery);
+Event.observe(window, 'load', checkForInitialQuery);
 
-function checkForQuery () {
-	if (lastLocation != window.location.href) {
-		lastLocation = window.location.href;
-		var prev = window.location.hash.split(":");
+function checkForInitialQuery() {
+  var argument = window.location.search.gsub(/\+/, '%20').toQueryParams()['query'];
 
-		if (prev.length == 2) {
-			$("querytext").value = prev[1];
-			processQuery(parseInt(prev[0].substr(1)));
-		}
-	}
-
-	var argument = window.location.search.gsub(/\+/, '%20').toQueryParams()['query'];
-	
-	if (argument) {
-		$("querytext").value = argument;
-		processQuery(0);
-	}
-
-	window.setTimeout(checkForQuery, 200);
+  if (argument) {
+    $("querytext").value = argument;
+    processQuery(0);
+  }
 }
 
-Event.observe(window.location, 'change', checkForQuery);
+function checkForQuery () {
+  if (lastLocation != window.location.hash) {
+    lastLocation = window.location.hash;
+    var prev = window.location.hash.split(":");
+
+    if (prev.length == 2) {
+      $("querytext").value = decodeURIComponent(prev[1]);
+      processQuery(parseInt(prev[0].substr(1)));
+    }
+    else {
+      window.setTimeout(checkForQuery, 200);
+    }
+  }
+  else {
+    window.setTimeout(checkForQuery, 200);
+  }
+}
 
 function tryProcessQuery () {
   var query = $("querytext").value;
-	window.setTimeout('checkProcessQuery(\'' + encodeURIComponent(query) + '\')', 300);
+  window.setTimeout('checkProcessQuery(\'' + encodeURIComponent(query) + '\')', 300);
 }
 
 function checkProcessQuery (query) {
-	if ((query != prevInput) && (query == encodeURIComponent($("querytext").value))) {
-		prevInput = query;
-		processQuery(0);
-	}
+  if ((query != prevInput) && (query == encodeURIComponent($("querytext").value))) {
+    prevInput = query;
+    processQuery(0);
+  }
 }
 
 function forceProcessQuery () {
-	processQuery(0);
-	
-	return false;
+  processQuery(0);
+  return false;
 }
 
 function processQuery (start) {
-	var query = $("querytext").value;
-	if (query.length > 0) {
-		$("throbber").show();
-		new Ajax.Request("results/hayoo.html?query=" + encodeURIComponent(query) + "&start=" + start,
-			{
-				method:'get',
-				onSuccess: function(transport) {
-					lastXMLResult = transport.responseXML;
-					lastTXTResult = transport.responseText;
-					lastQuery = query;
-					window.location.hash = start + ":" + encodeURIComponent(query);
-					lastLocation = window.location.href;
-				  displayResult(transport.responseText, query);
-				},
-				onFailure: function() {
-				  var resultError = "<div id=\"result\"><div id=\"status\">Error: Could not execute query.</div><div id=\"words\">&nbsp;</div><div id=\"documents\">&nbsp;</div><div id=\"pager\">&nbsp;</div></div>";		
-	
-				  $("result").replace(resultError);
-				}
-			}
-		);
-	}
+  var query = $("querytext").value;
+  if (query.length > 0) {
+    $("throbber").show();
+    new Ajax.Request("results/hayoo.html?query=" + encodeURIComponent(query) + "&start=" + start,
+      {
+        method:'get',
+        onSuccess: function(transport) {
+          lastXMLResult = transport.responseXML;
+          lastTXTResult = transport.responseText;
+          lastQuery = query;
+          window.location.hash = start + ":" + encodeURIComponent(query);
+          lastLocation = window.location.hash;
+          displayResult(transport.responseText, query);
+          checkForQuery();
+        },
+        onFailure: function() {
+          var resultError = "<div id=\"result\"><div id=\"status\">Error: Could not execute query.</div><div id=\"words\">&nbsp;</div><div id=\"documents\">&nbsp;</div><div id=\"pager\">&nbsp;</div></div>";
+          $("result").replace(resultError);
+        }
+      }
+    );
+  }
 }
 
 function displayResult (result, query) {
-	$("result").replace(result);
-	$("querytext").defaultValue = query;
-	$("throbber").hide();
+  $("result").replace(result);
+  $("querytext").defaultValue = query;
+  $("throbber").hide();
 }
 
 function replaceInQuery (needle, substitute) {
-	$("querytext").value = $("querytext").value.gsub(needle, substitute);
-	processQuery(0);
+  $("querytext").value = $("querytext").value.gsub(needle, substitute);
+  processQuery(0);
 }
 
 function showPage (page) {
-	if ($("querytext").value == "") {
-		$("querytext").value = lastQuery;
-	}
-	processQuery (page);
+  if ($("querytext").value == "") {
+    $("querytext").value = lastQuery;
+  }
+  processQuery (page);
 }
