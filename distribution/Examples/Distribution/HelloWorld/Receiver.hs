3f45a3ee8381abe05b418d698d961c1896d96c3b
sts <sts@holumbus.org>
2009-08-29 16:03:05 +0200

    HelloWorld example for the use of DStreamPort
    Ignore-this: 85e8bc88ab81bfc7a18ce7dfcd4ce15d
    
    darcs-hash:20090829140305-4d271-4e1b3c8918a8cc384dca9818a9869ed34bbbb3b6.gz

---
+++ distribution/Examples/Distribution/HelloWorld/Receiver.hs
@@ -0,0 +1,40 @@
+-- ----------------------------------------------------------------------------
+
+{- |
+  Module     : Holumbus
+  Copyright  : Copyright (C) 2009 Stefan Schmidt
+  License    : MIT
+
+  Maintainer : Stefan Schmidt (stefanschmidt@web.de)
+  Stability  : experimental
+  Portability: portable
+  Version    : 0.1
+  
+-}
+
+-- ----------------------------------------------------------------------------
+
+module Main(main) where
+
+import           Holumbus.Distribution.DNode
+import           Holumbus.Distribution.DStreamPort
+import           Holumbus.Common.Logging
+
+main :: IO ()
+main
+  = do
+    -- we want some Holumbus specific logging
+    initializeLogging []
+    -- make a Haskell DNode, named "myReceiver" on the Port 7999
+    -- this only needs to be called once during the runtime of the program
+    initDNode $ (defaultDNodeConfig "myReceiver")
+      { dnc_MinPort = (fromInteger 7999), dnc_MaxPort = (fromInteger 7999) }
+    -- make a new DStream, named "myStream"
+    -- this stream can be used until you close it with "closeDStream"
+    stream <- newDStream "myStream"
+    -- wait for the next message, read it print it out to stdout
+    msg <- (receive stream)::(IO String)
+    putStrLn msg
+    -- we are behaving nicely and clean everything up before we leave
+    closeDStream stream
+    deinitDNode
---
+++ distribution/Examples/Distribution/HelloWorld/Sender.hs
@@ -0,0 +1,41 @@
+-- ----------------------------------------------------------------------------
+
+{- |
+  Module     : Holumbus
+  Copyright  : Copyright (C) 2009 Stefan Schmidt
+  License    : MIT
+
+  Maintainer : Stefan Schmidt (stefanschmidt@web.de)
+  Stability  : experimental
+  Portability: portable
+  Version    : 0.1
+  
+-}
+
+-- ----------------------------------------------------------------------------
+
+module Main(main) where
+
+import           Holumbus.Distribution.DNode
+import           Holumbus.Distribution.DStreamPort
+import           Holumbus.Common.Logging
+
+main :: IO ()
+main
+  = do
+    -- we want some Holumbus specific logging
+    initializeLogging []
+    -- make a Haskell DNode, we don't care about its name, so we leave it
+    -- blank. The system will generate a unique random name on its own.
+    -- this only needs to be called once during the runtime of the program
+    initDNode $ defaultDNodeConfig ""
+    -- we need to know how to address the receiver node, so we have to provide
+    -- its address, this only needs to be done once, or when the address of
+    -- the receiver changes (which will not happen in most applications)
+    addForeignDNode $ mkDNodeAddress "myReceiver" "localhost" (fromInteger 7999)
+    -- we make a new port, connected to "myStream" at the node "myReceiver"
+    port <- newDPort "myStream" "myReceiver"
+    -- send the messages
+    send port "Hello World"
+    -- we are behaving nicely and clean everything up before we leave
+    deinitDNode
