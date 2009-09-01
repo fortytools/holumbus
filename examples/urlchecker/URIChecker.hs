15e1edfe077556e2a6d9ee02a7e2f64a6b6e24a1
uwe <uwe@home>
2009-09-01 10:40:43 +0200

    urlchecker example extended
    darcs-hash:20090901084043-1eb86-380ee7b294d0b73fbc3a64b58ba937522664a5a7.gz

--- examples/urlchecker/URIChecker.hs
+++ examples/urlchecker/URIChecker.hs
@@ -6,20 +6,32 @@ module Main
 where
 
 import           URIChecker.Check
+import		 Control.Arrow
 
 -- ------------------------------------------------------------
 
 -- | the list of document trees to be checked
 
-sessions		:: [(String, URIClassList)]
-sessions		= [ ( "http://localhost/~si/"
+sessions,
+  fhwSessions,
+  fhw2Sessions,
+  localhostSessions,
+  hobelSessions,
+  holumbusSessions	:: [(String, URIClassList)]
+
+sessions		= map addDefaults $
+			  fhwSessions ++ fhw2Sessions ++ localhostSessions ++ hobelSessions ++ holumbusSessions
+			  where
+			  addDefaults = second (++ defaults)
+
+localhostSessions	= [ ( "http://localhost/~si/"
 			    , [ ("http://localhost/~si/.*/welcome[.]html", 		Manual)
 			      , ("http://localhost/~si/index*[.]html", 			Contents)
 			      , ("http://localhost/~si.*[.]html[?].*",      		Manual)
 			      , ("http://localhost/~si.*[.]html",			Exists)
 			      , ("http://localhost/~si/.*[.](gif|jpg|css|ico|pdf)",	Exists)
 			      , ("http://localhost/.*", 				Manual)
-			      ] ++ defaults
+			      ]
 			    )
 
 			    -- java lecture
@@ -31,9 +43,11 @@ sessions		= [ ( "http://localhost/~si/"
 			      , ("http://localhost/~si/.*",					Exists)
 			      , ("http://localhost/",						Ignore)
 			      , ("http://.*",							Exists)
-			      ] ++ defaults
+			      ]
 			    )
-			  , ( "http://www.fh-wedel.de/~si/vorlesungen/java/java.html"
+			  ]
+
+fhwSessions		= [ ( "http://www.fh-wedel.de/~si/vorlesungen/java/java.html"
 			    , [ ("http://www.fh-wedel.de/~si/vorlesungen/java/welcome.html",		Ignore)
 			      , ("http://www.fh-wedel.de/~si/vorlesungen/java/.*[?]VAR=0",		Ignore)
 			      , ("http://www.fh-wedel.de/~si/vorlesungen/java/.*/exec.html[?].*",	Illegal)
@@ -43,19 +57,7 @@ sessions		= [ ( "http://localhost/~si/"
 			      , ("http://www.fh-wedel.de/~si/vorlesungen/java/.*",			Contents)
 			      , ("http://www.fh-wedel.de/~si/.*",					Exists)
 			      , ("http://.*",								Exists)
-			      ] ++ defaults
-			    )
-			  , ( "http://192.168.2.11/~si/vorlesungen/java/java.html"
-			    , [ ("http://192.168.2.11/~si/vorlesungen/java/welcome.html",		Ignore)
-			      , ("http://192.168.2.11/~si/vorlesungen/java/.*[?]VAR=0",			Ignore)
-			      , ("http://192.168.2.11/~si/vorlesungen/java/.*/exec.html[?].*",		Illegal)
-			      , ("http://192.168.2.11/~si/vorlesungen/java/.*/exec.html[?].*",		Illegal)
-			      , ("http://192.168.2.11/~si/vorlesungen/java/.*/program.html[?].*[.]html",		Contents)
-			      , ("http://192.168.2.11/~si/vorlesungen/java/.*/download[a-zA-Z0-9]*.html[?].*SRC=.*",	Exists)
-			      , ("http://192.168.2.11/~si/vorlesungen/java/.*",				Contents)
-			      , ("http://192.168.2.11/~si/.*",						Exists)
-			      , ("http://.*",								Exists)
-			      ] ++ defaults
+			      ]
 			    )
 			    -- FP lecture
 			  , ( "http://www.fh-wedel.de/~si/vorlesungen/fp/fp.html"
@@ -69,7 +71,7 @@ sessions		= [ ( "http://localhost/~si/"
 			      , ("http://www.fh-wedel.de/~si/vorlesungen/fp/.*",			Contents)
 			      , ("http://www.fh-wedel.de/~si/.*",					Exists)
 			      , ("http://.*",								Exists)
-			      ] ++ defaults
+			      ]
 			    )
 			    -- hxt home
 			  , ( "http://www.fh-wedel.de/~si/HXmlToolbox/hdoc/index.html"
@@ -78,10 +80,25 @@ sessions		= [ ( "http://localhost/~si/"
 			      , ("http://www.fh-wedel.de/usr/.*[.]html",				Ignore)		-- the default location for base documentation
 			      , ("http://hackage.haskell.org/packages/archive/base/latest/doc/html/.*",	Exists)		-- the location on hackage for base
 			      , ("http://.*",								Exists)
-			      ] ++ defaults
+			      ]
 			    )
-			    -- holumbus site
-			  , ( "http://holumbus.fh-wedel.de/"
+			  ]
+
+hobelSessions		= [ ( "http://192.168.2.11/~si/vorlesungen/java/java.html"
+			    , [ ("http://192.168.2.11/~si/vorlesungen/java/welcome.html",		Ignore)
+			      , ("http://192.168.2.11/~si/vorlesungen/java/.*[?]VAR=0",			Ignore)
+			      , ("http://192.168.2.11/~si/vorlesungen/java/.*/exec.html[?].*",		Illegal)
+			      , ("http://192.168.2.11/~si/vorlesungen/java/.*/exec.html[?].*",		Illegal)
+			      , ("http://192.168.2.11/~si/vorlesungen/java/.*/program.html[?].*[.]html",		Contents)
+			      , ("http://192.168.2.11/~si/vorlesungen/java/.*/download[a-zA-Z0-9]*.html[?].*SRC=.*",	Exists)
+			      , ("http://192.168.2.11/~si/vorlesungen/java/.*",				Contents)
+			      , ("http://192.168.2.11/~si/.*",						Exists)
+			      , ("http://.*",								Exists)
+			      ]
+			    )
+			  ]
+
+holumbusSessions	= [ ( "http://holumbus.fh-wedel.de/"
 			    , [ ("http://holumbus.fh-wedel.de/trac",					Contents)
 			      , ("http://holumbus.fh-wedel.de.*",					Exists)
 			      , ("http://darcs2.fh-wedel.de.*",						Exists)
@@ -91,9 +108,11 @@ sessions		= [ ( "http://localhost/~si/"
 			      , ("http://darcs.net.*",							Exists)
 			      , ("http://www.opensource.org/licenses/mit-license.php",			Exists)
 			      , ("http://.*.edgewall.org.*",						Exists)
-			      ] ++ defaults
+			      ]
 			    )
-			  , ( "http://www2.fh-wedel.de/"
+			  ]
+
+fhw2Sessions		= [ ( "http://www2.fh-wedel.de/"
 			    , [ ("http://www2.fh-wedel.de/~splan/index.html[?].*",			Ignore)		-- queries are not interesting
 			      , ("http://www2.fh-wedel.de/~cwlan/.*",					Contents)	-- cwlan home pages
 			      , ("http://www2.fh-wedel.de/~.*",						Exists)		-- personal home pages
@@ -102,7 +121,7 @@ sessions		= [ ( "http://localhost/~si/"
 			      , ("http://www2.fh-wedel.de/.*[?].*",					Exists)		-- pages with query string
 			      , ("http://www2.fh-wedel.de(/.*)?",					Contents)	-- "real" pages
 			      , ("http://.*",								Exists)
-			      ] ++ defaults
+			      ]
 			    )
 			  ]
 
@@ -110,7 +129,8 @@ sessions		= [ ( "http://localhost/~si/"
 defaults		:: URIClassList
 defaults		= [ ("http:.*",			Manual)
 			  , ("https:.*", 		Manual)
-			  -- , ("mailto:si@fh-wedel.de",	Ignore)
+			  , ("mailto:si@fh-wedel.de",	Ignore)
+			  , ("mailto:uwe@fh-wedel.de",	Ignore)
 			  , ("mailto:.*", 		Manual)
 			  , ("news:.*",   		Manual)
 			  , ("ftp:.*",			Manual)
