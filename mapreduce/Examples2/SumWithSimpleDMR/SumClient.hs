f11e24ee01b2dcf0e688ee23cfc1267205cdaee8
str <str@holumbus.org>
2009-09-18 11:45:32 +0200

    partition strategy again
    Ignore-this: 97d6cdb409ff35336277e1b3be27ab3b
    
    darcs-hash:20090918094532-f5258-dadfb65da93d4c2519b68922a0b51bedcb0f31d9.gz

--- mapreduce/Examples2/SumWithSimpleDMR/SumClient.hs
+++ mapreduce/Examples2/SumWithSimpleDMR/SumClient.hs
@@ -17,4 +17,17 @@ main = do
   putStrLn . show . length $ result
   putStrLn . show . sum . map snd $ result
   where
-  ls num =[(x`mod`num,[0+10000*x..9999+10000*x])|x<-[0..99]] -- == [[0..9999],[10000..19999],..,[y..999999]]
+  ls num = zip [0..] $ partition' [0..9999999] [[] |_<- [1..num]] -- [(x`mod`num,[0+10000*x..9999+10000*x])|x<-[0..99]] -- == [[0..9999],[10000..19999],..,[y..999999]]
+
+{-
+
+partition'
+
+first list, list of 
+ -}
+partition' :: [a] -> [[a]] -> [[a]]
+partition' _   [] = []
+partition' [] xss = xss
+partition' (u:us) (xs:xss) = partition' us (xss ++ [xs'])
+  where xs' = (u:xs)
+^ ^ ^ ^ ^ ^ ^
