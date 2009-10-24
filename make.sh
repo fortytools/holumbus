aa6ae3be8549c9362bab3cba608449ce6e57a456
str <str@holumbus.org>
2009-10-24 13:26:21 +0200

    added cabal instructions
    Ignore-this: 688cd86591676a8717f5f46ee2516385
    
    darcs-hash:20091024112621-f5258-07d244e2edd470249a948546388d50d7c21f0653.gz

--- make.sh
+++ make.sh
@@ -5,19 +5,24 @@ PROFILE="--enable-library-profiling --enable-executable-profiling"
 
 function doIt() {
   DIR=$1
-
+  PROF=""
+  [ "$2" = "prof" ] && PROF="-p --enable-executable-profiling"
+  echo $PROF
   cd $DIR && \
-  runhaskell Setup.hs configure $PROFILE && \
-  runhaskell Setup.hs build  && \
-  sudo runhaskell Setup.hs install --global
+  sudo cabal clean && \
+  sudo cabal install --global $PROF
+#  runhaskell Setup.hs clean && \
+#  runhaskell Setup.hs configure $PROFILE && \
+#  runhaskell Setup.hs build  && \
+#  sudo runhaskell Setup.hs install --global
   cd $BASE
 }
 
 if [ -z $1 ]
 then
-  doIt $BASE/distribution/ && \
-  doIt $BASE/storage/ && \
-  doIt $BASE/mapreduce/
+  doIt $BASE/distribution/ $2 && \
+  doIt $BASE/storage/ $2 && \
+  doIt $BASE/mapreduce/ $2
 else
-  doIt $1
+  doIt $1 $2
 fi
