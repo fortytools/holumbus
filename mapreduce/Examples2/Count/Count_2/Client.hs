6822012d9a2f61d45092a60e8c367f6565d44a69
Sebastian Reese <str@holumbus.org>
2009-11-22 16:20:20 +0100

    removed obsolete example

--- mapreduce/Examples2/Count/Count_2/Client.hs
+++
@@ -1,6 +0,0 @@
-module Main (main) where
-
-import Holumbus.MapReduce.Examples.Count.DCount 
-
-main :: IO ()
-main = client
--- mapreduce/Examples2/Count/Count_2/scripts/worker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR=$1
-cd $DIR 
-./Worker $2 logfile +RTS -K4G -N3 -sMemory.txt -RTS &
-echo $! > Worker.pid
