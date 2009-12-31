3c3ab79d7d659c6134b14b6f07e711963761c6f2
Sebastian Reese <str@holumbus.org>
2009-12-31 10:32:33 +0100

    Count_3 uses length instead of sum in reduce phase (faster?)

---
+++ mapreduce/Examples2/Count/Count_3/source/Holumbus/MapReduce/Examples/Count/DCount.hs
@@ -0,0 +1,42 @@
+module Holumbus.MapReduce.Examples.Count.DCount
+(
+   countMap
+ , countReduce
+ , K1
+ , V1
+)
+where
+
+import Holumbus.Distribution.SimpleDMapReduceIO
+import Control.Parallel.Strategies
+import Holumbus.MapReduce.Types
+import Data.Maybe (mapMaybe)
+
+type Options = [String]
+type K1 = Int
+type V1 = [String]
+type K2 = Int
+type V2 = Int
+type V3 = V4
+type V4 = Int
+
+{-
+
+type MapFunction a k1 v1 k2 v2 = ActionEnvironment -> a -> k1 -> v1 -> IO [(k2, v2)]
+-}
+countMap :: MapFunction Options K1 V1 K2 V2
+countMap _env wordsToCount k1 v1 = (return . zip (repeat k1) . mapMaybe counts) v1
+--   let cs= map (\x -> let x' = counts x in (k1,x')) v1 in rnf cs `seq` return cs
+  where
+  counts :: String -> Maybe Int
+  counts word = (b2int . or . map (==word)) wordsToCount
+  b2int True  = Just 1
+  b2int False = Nothing
+  
+
+{-
+
+type ReduceFunction a k2 v2 v3 = ActionEnvironment -> a -> k2 -> [v2] -> IO (Maybe v3)
+-}
+countReduce :: ReduceFunction Options K2 V2 V4
+countReduce _env _opts _k2 = return . Just . length
