5f33e4513183484e5ce072c36f41e513fffe1957
Thorben <thorbenguelck@gmx.net>
2011-10-08 14:45:51 +0200

    cronjob script update

--- indexJob.sh
+++ indexJob.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-# cd /home/administrator/W3W/index && make whole && killall webserver && cd /home/administrator/W3W && /home/administrator/.cabal/bin/webserver -p 80 > out.txt 2> err.txt &
-# echo "Created new index: "`date` >> /home/administrator/W3W/indexJob.log
+cd /home/administrator/W3W
+echo "Creating index: "`date` >> indexJob.log
+cd index
+mv Makefile .. && rm -rf * && mv ../Makefile .
+make whole
 
-killall webserver && cd /home/administrator/W3W && /home/administrator/.cabal/bin/webserver -p 80 > out.txt 2> err.txt &
-echo "Restarted webserver: "`date` >> /home/administrator/W3W/indexJob.log
+cd ..
+echo "Restarting webserver: "`date` >> indexJob.log
+sudo killall webserver
+sudo ../.cabal/bin/webserver -p 80 &
