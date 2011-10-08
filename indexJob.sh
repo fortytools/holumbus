52253414fd59f08265af42732767964015c802e0
Thorben <thorbenguelck@gmx.net>
2011-10-08 14:57:32 +0200

    added index size logging

--- indexJob.sh
+++ indexJob.sh
@@ -7,6 +7,8 @@ mv Makefile .. && rm -rf * && mv ../Makefile .
 make whole
 
 cd ..
+echo "Index size: "`du -s ./index | sed -e "s/\(^[^\t]*\)\t.*$/\1/"`" MB" >> indexJob.log
+
 echo "Restarting webserver: "`date` >> indexJob.log
 sudo killall webserver
 sudo ../.cabal/bin/webserver -p 80 &
