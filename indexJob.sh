5324c4eaca6fcd5b429a8e4157173f87ae11d523
thorben <thorben@tglaptop-linux.(none)>
2011-10-12 10:38:33 +0200

    updated indexJob

--- indexJob.sh
+++ indexJob.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 cd /home/administrator/W3W
-echo "Creating index: "`date` >> indexJob.log
+echo "Creating index: "`date` >> log/indexJob.log
 cd index
 mv Makefile .. && rm -rf * && mv ../Makefile .
 make whole
 
 cd ..
-echo "Index size: "`du -s ./index | sed -e "s/\(^[^\t]*\)\t.*$/\1/"`" MB" >> indexJob.log
+echo "Index size: "`du -s ./index | sed -e "s/\(^[^\t]*\)\t.*$/\1/"`" MB" >> log/indexJob.log
 
-echo "Restarting webserver: "`date` >> indexJob.log
+echo "Restarting webserver: "`date` >> log/indexJob.log
 sudo killall webserver
-sudo ../.cabal/bin/webserver -p 80 &
+sudo ../.cabal/bin/webserver -p 80 > log/out.log 2> log/err.log &
