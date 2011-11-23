0f70036ece0c94689e6f987efcf2c87dd719164e
fhsearch <Hydralisc@gmx.net>
2011-11-23 16:50:57 +0100

    Update indexJob.sh

--- indexJob.sh
+++ indexJob.sh
@@ -6,9 +6,7 @@ mkdir log
 echo "Creating index: "`date` >> log/indexJob.log
 cd index
 make whole 2>&1 >> ../log/indexJob.log
-
 cd ..
-echo "Index size: "`du -s ./index | sed -e "s/\(^[^\t]*\)\t.*$/\1/"`" MB" >> log/indexJob.log
 
 echo "Restarting webserver: "`date` >> log/indexJob.log
 sudo killall apache2
