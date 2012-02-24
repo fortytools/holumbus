b51ab9c1488d752b9860d4f280f1368b4a3b399f
Emil Steinberger <emil@delle.fh-wedel.de>
2012-02-24 15:08:38 +0100

    in W3W genScripts modified

--- W3W/genScripts.sh
+++ W3W/genScripts.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+port=${1:-80}
 pwd=$(pwd)
 
 cat > ./cronfile <<EOF
 0 2 * * * $pwd/indexJob.sh
 EOF
+echo "created: ./cronfile" 1>&2
 
 cat > ./installCronjob.sh <<EOF
 crontab cronfile
 EOF
 chmod a+x ./installCronjob.sh
+echo "created: ./installCronjob.sh" 1>&2
 
 cat > ./indexJob.sh <<EOF
 #! /bin/bash
@@ -19,6 +22,8 @@ cd $pwd
 
 echo "Creating index: "\$(date) >> log/indexJob.log
 cd index
+[ -d "cache" ] || mkdir cache
+[ -d "tmp"   ] || mkdir tmp
 make whole 2>&1 >> ../log/indexJob.log
 cd ..
 
@@ -27,6 +32,7 @@ sudo killall apache2
 
 sudo killall w3wServer
 sleep 2
-sudo $HOME/.cabal/bin/w3wServer -p 80 > log/out.log 2> log/err.log &
+sudo $HOME/.cabal/bin/w3wServer -p $port > log/out.log 2> log/err.log &
 EOF
 chmod a+x ./indexJob.sh
+echo "created: ./indexJob.sh" 1>&2
